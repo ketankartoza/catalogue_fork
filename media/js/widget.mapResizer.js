e86dcf1e6debb7d0178b4b14e7527eb72add2173
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2011-02-05 19:32:35 +0100

    improved resizer behaviour, added functionality fullscreen/restore map functionality

--- media/js/widget.mapResizer.js
+++ media/js/widget.mapResizer.js
@@ -1,28 +1,58 @@
+/*
+ * MapResizer widget - enables users to resize map to arbitary size
+ * @author: Dražen Odobašić <dodobasic@gmail.com>
+ *
+ * Options:
+ *
+ * map - mandatory, reference to OpenLayers map object
+ *
+ * scrollTo - if set browser is scrolled to top of the map
+ *  - optional, default true
+ *
+ * map_id - id of div which contains OL map
+ *  - optional, default 'map'
+ *
+ * map_panel_id - id of div containg map tools, scale, ...
+ *  - optional, default 'map-panel'
+ */
+
 (function($) {
     $.widget("sansa.mapResizer",{
 	
 	// default options
 	options: {
-	    'map': null
+	    'map': null,
+	    'scrollTo':true,
+	    'map_id':'map',
+	    'map_panel_id':'map-panel'
 	},
 
 	// creation code for mywidget
 	// can use this.options
 	_create: function() {
 	    var self=this;
+	    if (!this.options.map){
+		$.error('Map object is mandatory');
+	    }
 	    
 	    //calculate minimal right edge for the map
 	    var elem_parent= this.element.parent();
 	    this.right_edge_limit = elem_parent.offset().left + elem_parent.width();
 
-	    //set min_width to parent width
+	    //set min_width to parent width and height
 	    this.min_width=elem_parent.width();
+	    this.min_height=elem_parent.height();
 
-	    this.map=this.element.find("#map");
+	    //set map_panel_height +2 (for borders)
+	    this.map_panel_height=this.element.find("#"+this.options.map_panel_id).height()+2;
+	    this.map=this.element.find("#"+this.options.map_id);
+
+	    //add toolbox
+	    this.element.append(this._toolbox());
 	    
 	    //this.element.css({'position':'relative'});
 	    this.element.resizable({
-		minHeight: 300,
+		minHeight: self.min_height,
 		minWidth: self.min_width,
 		
 		start: function (evt){
@@ -30,7 +60,7 @@
 		},
 		resize: function (evt){
 		    //called during resize
-		    self.map.height(self.element.height()-38);
+		    self.map.height(self.element.height()-self.map_panel_height);
 		    self.map.width(self.element.width());
 		},
 		stop: function (evt){
@@ -39,18 +69,12 @@
 		    
 		    var right_edge=offset.left+new_width;
 		    if (right_edge < self.right_edge_limit) {
-			self.element.offset({top:offset.top,left:self.right_edge_limit-self.min_width});
-			self.element.width(self.min_width);
+			self._set_dim({top:offset.top,left:self.right_edge_limit-self.min_width},self.min_width,self.element.height());
 		    }
 		    else {
 			var left_offset = new_width-self.old_width;//mirror left side as much as right side == mirror
-			var final_width=new_width+left_offset;
-			
-			self.element.offset({top:offset.top,left:offset.left-left_offset});
-			self.element.width(new_width+left_offset);
-			self.map.width(new_width+left_offset);
+			self._set_dim({top:offset.top,left:offset.left-left_offset},new_width+left_offset,self.element.height());
 		    }		    
-		    self._set_dim(); // resize map and recenter (mandatory for draw feature controls)			
 		}
 	    });
 	},
@@ -60,29 +84,70 @@
 	_init: function(){
 
 	},
+	// resize map and recenter (mandatory for draw feature controls)
+	_set_dim: function (offset,width,height){
+	    this.element.offset(offset);
+	    this.element.width(width);
+	    this.map.width(width);
+	    this.element.height(height);
+	    var map_height=height-this.map_panel_height;
+	    this.map.height(map_height);
 
-	_set_dim: function (offset){
-	    var center = self.options.map.getCenter();
-	    var zoom = self.options.map.getZoom();
+	    var center = this.options.map.getCenter();
+	    var zoom = this.options.map.getZoom();
 	    this.options.map.setCenter(center,zoom);
 	},
 
-	_fullscreen_image:function(arg){
+	_toolbox:function(arg){
 	    var self=this;
-	    var img_handle = $('<div>').attr({
-		'id':'map_resize'
+
+	    var toolbox = $('<div>').attr({
+		'id':'resizer_toolbox'
 	    });
-	    img_handle.css({
-		'z-index':99999,
-		'width':'32px',
-		'height':'32px',
-		'display':'none',
+	    toolbox.css({
+		'display':'block',
 		'position':'absolute',
-		'bottom':'0px',
-		'right':'0px',
-		'background-image':'url("/media/images/sun.png")'
+		'bottom':'5px',
+		'right':'10px'
 	    });
-	    return img_handle
+
+	    var fullscreen_image = $('<img>').attr({
+		'id':'fullscreen_img',
+		'title':'Resize map to full screen',
+		'src':"/media/images/view-fullscreen-4.png"
+	    });
+
+	    fullscreen_image.bind('click', function (evt){
+		var H=$(window).height();
+		var W=$(window).width();
+		var offset=self.element.offset();
+		//hardcoded decrements, to avoid browser horizontal scroll bar
+		self._set_dim({top:offset.top,left:5},W-15,H-5);
+		//scroll window to the top of the map
+		if (self.options.scrollTo){
+		    $(window).scrollTop(offset.top);
+		}
+	    });
+
+	    var restore_image = $('<img>').attr({
+		'id':'fullscreen_img',
+		'title':'Restore map to original size',
+		'src':"/media/images/view-restore-2.png"
+	    });
+
+	    restore_image.bind('click', function (evt){
+		var offset=self.element.offset();
+		self._set_dim({top:offset.top,left:self.right_edge_limit-self.min_width},self.min_width,self.min_height);
+		//scroll window to the top of the map
+		if (self.options.scrollTo){
+		    $(window).scrollTop(offset.top);
+		}
+	    });
+
+	    toolbox.append(restore_image);
+	    toolbox.append(fullscreen_image);
+
+	    return toolbox
 	}
 
     })//end widget
