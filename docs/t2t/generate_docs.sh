e2091acc5117fb020e4abdfae83d9a58b93cbbf9
Tim Sutton <tim@linfiniti.com>
2011-02-16 17:17:35 +0200

    Added table listings for various dictionaries

--- docs/t2t/generate_docs.sh
+++ docs/t2t/generate_docs.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 txt2tags -t tex -o SANSA-Catalogue-Manual.tex 001-index.t2t
 pdflatex SANSA-Catalogue-Manual.tex
-rm SANSA-Catalogue-Manual.aux  SANSA-Catalogue-Manual.log   SANSA-Catalogue-Manual.out  SANSA-Catalogue-Manual.tex  SANSA-Catalogue-Manual.toc
+
+#if you cleanup these temp files latex wont generate teh docs as it should
+#rm SANSA-Catalogue-Manual.aux  SANSA-Catalogue-Manual.log   SANSA-Catalogue-Manual.out  SANSA-Catalogue-Manual.tex  SANSA-Catalogue-Manual.toc
 
