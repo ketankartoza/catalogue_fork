d34706747ccba3517d32769b58018870189bc634
Tim Sutton <tim@linfiniti.com>
2011-02-16 16:37:05 +0200

    Added git image, fixed some markup errors, further tidy ups

--- docs/generate_docs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-txt2tags -t tex -o index.tex index.t2t; pdflatex index.tex
--- docs/t2t/generate_docs.sh
+++ docs/t2t/generate_docs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 txt2tags -t tex -o SANSA-Catalogue-Manual.tex 001-index.t2t
 pdflatex SANSA-Catalogue-Manual.tex
+rm SANSA-Catalogue-Manual.aux  SANSA-Catalogue-Manual.log   SANSA-Catalogue-Manual.out  SANSA-Catalogue-Manual.tex  SANSA-Catalogue-Manual.toc
 
