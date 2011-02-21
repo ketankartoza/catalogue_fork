1e6f6f7a0465c693657c53683afd51c1aa3e2530
Alessandro Pasotti <apasotti@gmail.com>
2011-02-21 19:48:02 +0100

    DIMS package reader-writer, first working implementation and tests

--- catalogue/dims_lib.py
+++ catalogue/dims_lib.py
@@ -5,7 +5,10 @@ and/or imagery and thumbnails
 
 import os
 import re
+import shutil
+import tempfile
 import tarfile
+import logging
 from datetime import datetime
 
 try:
@@ -14,13 +17,26 @@ except ImportError:
   import xml.etree.ElementTree as etree
 
 
-class dims(object):
+class MetadataAlreadyAddedException(Exception):
   """
-  The main class
+  Raised when a metadata for a given product_code is already present
   """
-  statuses    = ('OPEN', 'CLOSE')
+  pass
 
+class MetadataNotFoundException(Exception):
+  """
+  Raised when a mandatory metadata for a given product_code cannot be found
+  """
+  pass
+
+
+class dimsBase(object):
+  """
+  Stores common XML functions
+  """
+  # Namespaces
   NS = dict(
+      xmlns       = "{http://www.isotc211.org/2005/gmd}",
       xmlns_gco   = "{http://www.isotc211.org/2005/gco}",
       xmlns_gts   = "{http://www.isotc211.org/2005/gts}",
       xmlns_gss   = "{http://www.isotc211.org/2005/gss}",
@@ -28,23 +44,42 @@ class dims(object):
       xmlns_xsi   = "{http://www.w3.org/2001/XMLSchema-instance}",
       xmlns_xlink = "{http://www.w3.org/1999/xlink}",
       xmlns_gml   = "{http://www.opengis.net/gml}",
-      xmlns       = "{http://www.isotc211.org/2005/gmd}",
+    )
+
+  #TODO: missing from ISO 'spatial_coverage':     PolygonField
+  # Replaceable targets
+  METADATA = dict(
+      product_date            = '//{xmlns}dateStamp/{xmlns_gco}Date', # Product date
+      file_identifier         = '//{xmlns}fileIdentifier/{xmlns_gco}CharacterString',
+      vertical_cs             = '//{xmlns_gml}VerticalCS/{xmlns_gml}name', # Projection
+      processing_level_code   = '//{xmlns}processingLevelCode//{xmlns}code/{xmlns_gco}CharacterString',
+      cloud_cover_percentage  = '//{xmlns}cloudCoverPercentage/{xmlns_gco}Real',
+      md_data_identification  = '//{xmlns}MD_DataIdentification//{xmlns}CI_Citation/{xmlns}title/{xmlns_gco}CharacterString',
+      md_product_date         = '//{xmlns}MD_DataIdentification//{xmlns}CI_Date//{xmlns_gco}Date',
+      md_abstract             = '//{xmlns}MD_DataIdentification/{xmlns}abstract/{xmlns_gco}CharacterString', # Sat & sensor description
+      bbox_west               = '//{xmlns}EX_GeographicBoundingBox/{xmlns}westBoundLongitude/{xmlns_gco}Decimal',
+      bbox_east               = '//{xmlns}EX_GeographicBoundingBox/{xmlns}eastBoundLongitude/{xmlns_gco}Decimal',
+      bbox_north              = '//{xmlns}EX_GeographicBoundingBox/{xmlns}northBoundLatitude/{xmlns_gco}Decimal',
+      bbox_south              = '//{xmlns}EX_GeographicBoundingBox/{xmlns}southBoundLatitude/{xmlns_gco}Decimal',
     )
 
 
+class dimsReader(dimsBase):
+  """
+  This class, extracts metadata and thumbnails from an existing tar.gz package
+  """
+
   def __init__(self, path):
     """
-    Set the file path
+    Set the file path and reads the package
     """
-    import logging
-    logging.debug(self.NS)
+    logging.info("%s initialized" % self.__class__)
     self._path      = path
-    self._status    = None
     self._tar       = None
     self._tar_index = None
     self._products  = {}
-    if os.path.isfile(path):
-      self._read()
+    self._read()
+
 
   def _read(self):
     """
@@ -55,8 +90,9 @@ class dims(object):
 
     # Scan for DIMS products
     for product_path in filter(lambda x: re.search(self._tar_index[0] + '/' + 'Metadata/ISOMetadata/[^/]+/[^\.]+\.xml$', x), self._tar_index):
-      m = re.search('ISOMetadata/([^/]+)/([^/]+)$', product_path)
+      m = re.search('ISOMetadata/([^/]+)/([^/]+)\.xml$', product_path)
       processing_level_code, product = m.groups()
+      logging.info("reading %s" % product)
       self._products[product] = {
           'path':       product_path,
           'metadata':   self._read_metadata(product_path),
@@ -67,20 +103,22 @@ class dims(object):
   def _read_metadata(self, product_path):
     """
     Extract metadata from an XML metadata file object and
-    returns informations as a dictionary
+    returns informations as a dictionary, parsing and validation
+    is left to the calling program
     """
     file_info = self._tar.getmember(product_path)
     file_handle = self._tar.extractfile(file_info)
     tree = etree.parse(file_handle)
-    #TODO: missing from ISO 'spatial_coverage':     PolygonField
-    metadata = {
-          #'product_date':             datetime.strptime(tree.find('//{xmlns}dateStamp/{xmlns_gco}Date'.format(**self.NS)).text, '%Y-%m-%dT%H:%M:%S.%f'),
-          'product_date':             tree.find('//{xmlns}dateStamp/{xmlns_gco}Date'.format(**self.NS)).text,
-          'projection' :              tree.find('//{xmlns_gml}VerticalCS/{xmlns_gml}name'.format(**self.NS)).text,
-          'processing_level_code':    tree.find('//{xmlns}processingLevelCode//{xmlns}code/{xmlns_gco}CharacterString'.format(**self.NS)).text,
-          'cloud_cover_percentage':   tree.find('//{xmlns}cloudCoverPercentage/{xmlns_gco}Real'.format(**self.NS)).text,
-        }
+    metadata = {}
+    for md_name, md_xpath in self.METADATA.items():
+      logging.info('searching for %s in path %s' % (md_name, md_xpath))
+      try:
+        metadata[md_name] = tree.find(md_xpath.format(**self.NS)).text
+      except AttributeError:
+        logging.warning('not found %s in path %s' % (md_name, md_xpath))
+    logging.info("metadata: %s" % metadata)
     return metadata
+
 
   def _read_file(self, file_path):
     """
@@ -90,22 +128,166 @@ class dims(object):
     return self._tar.extractfile(file_info)
 
 
-  def get_metadata(self):
+  def get_products(self):
     """
-    Returns metadata
+    Returns all read data
     """
     return self._products
 
-  def write(self):
+  def get_metadata(self, product_code):
     """
-    Write the tarfile to disk
+    Returns metadata for the given product_code
     """
-    pass
+    return self._products.get(product_code).get('metadata')
 
   def __str__(self):
     """
     Print
     """
-    return 'DIMS [%s]: %s ' % (self._status, self._path)
+    return 'DIMS Reader: %s ' % self._path
 
 
+
+
+class dimsWriter(dimsBase):
+  """
+  This class takes a folders containing the package template and fill in the gaps.
+  """
+
+  def __init__(self, template_path, package_name):
+    """
+    Set the file path and reads the package
+    """
+    logging.info("%s initialized" % self.__class__)
+    self._tmp           = tempfile.mkdtemp()
+    self._path          = os.path.join(self._tmp, package_name)
+    self._package_name  = package_name
+    self._xml_template  = os.path.join(self._path, 'Metadata', 'ISOMetadata', 'ISOMetadata_template.xml')
+    self._xml           = {}
+    # makes a temporary copy of the template folder, all processing will be done on the copy
+    shutil.copytree(template_path, self._path)
+    logging.info("template copied to %s" % self._path)
+
+
+  def _create_processing_level_folders(self, processing_level_code):
+    """
+    Creates the processing levels folders
+    """
+    for p in (os.path.join('Metadata', 'ISOMetadata'), os.path.join('Metadata', 'Thumbnails')):
+      _p = os.path.join(self._path, p, processing_level_code)
+      if not os.path.isdir(_p):
+        logging.info("creating %s" % _p)
+        os.mkdir(_p)
+
+  def _get_metadata(self, product_data, key, silently_fail=False):
+    """
+    Returns a metadata value for a given product_code
+    """
+    try:
+      product_data['metadata']
+    except KeyError:
+      MetadataNotFoundException('metadata are completely missing from product data')
+    try:
+      return product_data['metadata'][key]
+    except KeyError:
+      if silently_fail:
+        return None
+      raise MetadataNotFoundException('%s not found' % key)
+
+  def _add_metadata(self, product_code, product_data):
+    """
+    Set metadata in the template
+    """
+    # Metadata
+    _xml = os.path.join(self._path, 'Metadata', 'ISOMetadata', self._get_metadata(product_data, 'processing_level_code'), product_code + '.xml')
+    tree = etree.parse(self._xml_template)
+    metadata = {}
+    for md_name, md_xpath in self.METADATA.items():
+      logging.info('searching for %s in path %s' % (md_name, md_xpath))
+      try:
+        _val =  self._get_metadata(product_data, md_name, True)
+        try:
+          tree.find(md_xpath.format(**self.NS)).text = _val
+          logging.info("adding %s = %s" % (md_name, _val))
+        except AttributeError:
+          logging.warning('not found %s in path %s' % (md_name, md_xpath))
+      except MetadataNotFoundException:
+        print 'searching for %s in path %s' % (md_name, md_xpath)
+    self._xml[product_code] = _xml
+    tree.write(_xml)
+    logging.info("adding metadata to %s" % _xml)
+
+
+  def _add_product(self, product_code, product_data):
+    """
+    Adds a single product
+    """
+    # Check if already added
+    if product_code in self._xml:
+      raise MetadataAlreadyAddedException
+    self._create_processing_level_folders(self._get_metadata(product_data, 'processing_level_code'))
+    # Adds the thumbnail, mandatory
+    shutil.copy(product_data['thumbnail'], os.path.join(self._path, 'Metadata', 'Thumbnails', self._get_metadata(product_data, 'processing_level_code'), product_code + '.jpg'))
+    # The product imagery itself is not mandatory
+    try:
+      if product_data['product_image']:
+        # make sure the folders exists
+        _p = os.path.join(self._path, 'Products', 'SacPackage', self._get_metadata(product_data, 'verticalcs_name'),  self._get_metadata(product_data, 'processing_level_code') + '_DIMAP')
+        try:
+          os.makedirs(_p)
+        except OSError:
+          logging.warning('cannot create %s' % _p)
+        # Copy the image, rename the file to match product_code and keeps extension
+        _image_path = os.path.join(_p, product_code + os.path.splitext(product_data['product_image'])[1])
+        shutil.copy(product_data['product_image'], _image_path)
+        logging.info("image saved in %s" % _image_path)
+    except KeyError:
+        logging.warning('no image data for %s' % product_code)
+
+    self._add_metadata(product_code, product_data)
+
+
+  def add_products(self, products_info):
+    """
+    Adds products to the archive directory.
+
+    product_info must contain all the informations
+    needed to create the product informations,
+    files informations are passed as paths to the files
+    """
+    for product_code, product_data in products_info.items():
+      self._add_product(product_code, product_data)
+
+  def _get_tarball_name(self):
+    """
+    Returns the tarball name
+    """
+    return self._package_name + '.tar.gz'
+
+  def _write(self):
+    """
+    Creates the package
+    """
+
+    #TODO: dump the XML
+
+    os.chdir(os.path.split(self._path)[0])
+    tar_path = os.path.join(os.getcwd(), self._get_tarball_name())
+    tar = tarfile.open(self._get_tarball_name(), "w:gz")
+    tar.add(self._package_name, exclude = lambda x: -1 != x.find('ISOMetadata_template'))
+    tar.close()
+    logging.info("writing tarball %s" % tar_path)
+    return tar_path
+
+  def write(self):
+    """
+    Public api
+    """
+    return self._write()
+
+
+  def __str__(self):
+    """
+    Print
+    """
+    return 'DIMS Writer: %s ' % self._path
--- catalogue/tests/dims_test.py
+++ catalogue/tests/dims_test.py
@@ -5,11 +5,86 @@ Unit tests for DIMS
 
 __test__ = {"doctest": """
 
+
+Tests reader
+
 >>> import os
->>> from catalogue.dims_lib import dims
->>> d = dims(os.path.join(os.path.split(__file__)[0], 'dims_packages', 'ORD_420882_20110124_20110124_SPOT-_V01_1.tar.gz'))
->>> d.get_metadata() # doctest:+ELLIPSIS
-{'S5-_HRG_B--_CAM2_0094_00_0367_00_110122_092557_L1A-_ORBIT-.xml': {'path': 'ORD_420882_20110124_20110124_SPOT-_V01_1/Metadata/ISOMetadata/DN_L1A/S5-_HRG_B--_CAM2_0094_00_0367_00_110122_092557_L1A-_ORBIT-.xml', 'thumbnail': <tarfile.ExFileObject object at ...>, 'metadata': {'product_date': '2011-01-24T14:29:43.278', 'cloud_cover_percentage': None, 'projection': 'ORBIT', 'processing_level_code': 'L1A'}}}
+>>> from catalogue.dims_lib import dimsWriter, dimsReader
+>>> d = dimsReader(os.path.join(os.path.split(__file__)[0], 'dims_packages', 'ORD_420882_20110124_20110124_SPOT-_V01_1.tar.gz'))
+>>> products = d.get_products()
+>>> product_code = products.keys()[0]
+>>> products[product_code]['thumbnail'] # doctest:+ELLIPSIS
+<tarfile.ExFileObject object at ...>
+
+>>> md = d.get_metadata(product_code)
+>>> md.get('bbox_east') == '13.778910'
+True
+>>> md.get('bbox_north') == '-8.840805'
+True
+>>> md.get('bbox_south') == '-8.190087'
+True
+>>> md.get('bbox_west') == '13.121074'
+True
+>>> md.get('cloud_cover_percentage') == None
+True
+>>> md.get('file_identifier') == 'S5-_HRG_B--_CAM2_0094_00_0367_00_110122_092557_L1A-_ORBIT--Vers.0.01'
+True
+>>> md.get('md_abstract').find('The SPOT high resolution optical') != -1
+True
+>>> md.get('md_product_date') == '2011-01-22T09:25:57.000'
+True
+>>> md.get('processing_level_code') == 'L1A'
+True
+>>> md.get('product_date') == '2011-01-24T14:29:43.278'
+True
+>>> md.get('vertical_cs') == 'ORBIT'
+True
+>>> md.get('md_data_identification') == 'SPOT5.HRG.L1A'
+True
+
+Tests writer
+
+>>> d  = dimsWriter('catalogue/tests/dims_template', '0000000001')
+
+>>> products = {}
+>>> products['PRODUCT_00001'] = {}
+>>> products['PRODUCT_00001']['thumbnail']= 'catalogue/tests/sample_files/sample_thumbnail.jpg'
+>>> products['PRODUCT_00001']['product_image'] = 'catalogue/tests/sample_files/sample_image.tif'
+>>> products['PRODUCT_00001']['metadata'] = {}
+>>> products['PRODUCT_00001']['metadata']['product_date'] = '2011-01-24T14:29:43.278'
+>>> products['PRODUCT_00001']['metadata']['md_data_identification'] = 'SPOT5.HRG.L1A'
+>>> products['PRODUCT_00001']['metadata']['processing_level_code'] = 'DN_L1A'
+>>> products['PRODUCT_00001']['metadata']['verticalcs_name'] = 'ORBIT'
+>>> d.add_products(products)
+>>> tar_path = d.write()
+>>> os.path.split(tar_path)[1]
+'0000000001.tar.gz'
+
+Now tests that the test image and thumbnail are really in the tarball
+
+>>> import tarfile
+>>> t = tarfile.open(tar_path, 'r:gz')
+>>> '0000000001/Products/SacPackage/ORBIT/DN_L1A_DIMAP/PRODUCT_00001.tif' in t.getnames()
+True
+>>> '0000000001/Products/SacPackage/ORBIT/DN_L1A_DIMAP/PRODUCT_00001.tif' in t.getnames()
+True
+>>> '0000000001/Metadata/ISOMetadata/DN_L1A/PRODUCT_00001.xml' in t.getnames()
+True
+>>> '0000000001/Metadata/Thumbnails/DN_L1A/PRODUCT_00001.jpg' in t.getnames()
+True
+>>> '0000000001/Metadata/ISOMetadata/ISOMetadata_template.xml' in  t.getnames()
+False
+
+
+Test XML metadata export
+
+>>> from lxml import etree
+>>> xml = etree.parse(os.path.join( tar_path[:-7], 'Metadata/ISOMetadata/DN_L1A/PRODUCT_00001.xml'))
+>>> xml.find('//{http://www.isotc211.org/2005/gmd}dateStamp/{http://www.isotc211.org/2005/gco}Date').text
+'2011-01-24T14:29:43.278'
+>>> xml.find('//{http://www.isotc211.org/2005/gmd}MD_DataIdentification//{http://www.isotc211.org/2005/gmd}CI_Citation/{http://www.isotc211.org/2005/gmd}title/{http://www.isotc211.org/2005/gco}CharacterString').text
+'SPOT5.HRG.L1A'
+
 
 
 """}
