66d6a911068237c111e5aa23ed12ae5ca3cd6e27
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2011-02-23 14:04:37 +0100

    added script to load world borders geometry

---
+++ scripts/load_world_borders.py
@@ -0,0 +1,8 @@
+from django.contrib.gis.utils import LayerMapping
+from catalogue.models import WorldBorders,world_borders_mapping
+
+SHAPE_FILE='../TM_WORLD_BORDERS-0.3.shp'
+
+lm = LayerMapping(WorldBorders, SHAPE_FILE, world_borders_mapping,encoding='latin-1')
+
+lm.save()
