065139e72c66c5ffe3c2580109c983ff126d369b
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2011-03-15 10:10:31 +0100

    rewritten logic for utmzonecalc

--- catalogue/utmzonecalc.py
+++ catalogue/utmzonecalc.py
@@ -1,34 +1,37 @@
+#calcualte center meridians of utmzones for the world
+#we use list.index(center_meridian)+1 as UTM zone number
+utmzones = [x*6+3 for x in range(-30,30)]
 
-#determine utm zone based on lat,lon
-def utmZoneFromLatLon(theLon, theLat, theBuffer = 1):
-  """Return the name of the UTM zone given a latitude and longitude.
-     @return a tuple e.g. 32734, UTM34S"""
-  mySuffix = None
-  myPrefix = "326" #all north utm CRS's begin with 327
-  myHemisphere = "N"
-  if theLat < 0:
-    myPrefix = "327"
-    myHemisphere = "S"
-  myZoneMin = -180
-  myZoneIncrement = 6
-  myCurrentZone = 1
-  try:
-    while myZoneMin < 180:
-      if theLon >= myZoneMin and theLon < ( myZoneMin + 6 ):
-        myResult = []
-        mySuffix = "%02d" % myCurrentZone
-        myResult.append((myPrefix+mySuffix,"UTM"+mySuffix+myHemisphere))
-        #calcualte buffer
-        mySuffix = "%02d" % (myCurrentZone+1)
-        myResult.append((myPrefix+mySuffix,"UTM"+mySuffix+myHemisphere))
-        mySuffix = "%02d" % (myCurrentZone-1)
-        myResult.append((myPrefix+mySuffix,"UTM"+mySuffix+myHemisphere))
-        
-        return myResult
-      myZoneMin += myZoneIncrement
-      myCurrentZone += 1
-    print "Point out of range for UTM zone calculation"
-  except Exception, e:
-    print "Failed to define a new UTM zone for Lon: %s, Lat: %s" % ( theLon, theLat )
-    raise e
-  return None
+def utmZoneFromLatLon(theLon, theLat, theBuffer = 0):
+  if theLon <= -180 or theLon >= 180:
+    raise ValueError("Longitude value error: %d" % theLon)
+  if theLat <= -90 or theLat >= 90:
+    raise ValueError("Latitude value error: %d" % theLat)
+
+  #look up zone using simple calculation, add 1 because lists are 0 based
+  myZone = utmzones.index(int(theLon / 6)*6+3)+1
+
+  if theBuffer:
+    myZones=[]
+    for buf in range(-theBuffer,theBuffer+1):
+      tmp = myZone + buf
+      #account for zones on the edges
+      if tmp>60:
+        tmp=tmp-60
+      if tmp<1:
+        tmp=tmp+60
+      myZones.append(tmp)
+  else:
+    myZones = [myZone]
+
+  myResult = []
+  for zone in myZones:
+    if theLat > 0:
+      myHemisphere = 'N'
+      myPrefix = "326"
+    else:
+      myHemisphere = 'S'
+      myPrefix = "327"
+    mySuffix = "%02d" % zone
+    myResult.append((myPrefix+mySuffix,"UTM"+mySuffix+myHemisphere))
+  return myResult
