313690d89ca93e02a7ead024dae9ea2de63525b4
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2011-03-15 09:09:35 +0100

    updated projections text

--- sql/migrations/102-projections-insert.sql
+++ sql/migrations/102-projections-insert.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-INSERT INTO "catalogue_projection" ("epsg_code", "name") VALUES (4326,'EPSG4326');
-INSERT INTO "catalogue_projection" ("epsg_code", "name") VALUES (900913,'EPSG900913');
+INSERT INTO "catalogue_projection" ("epsg_code", "name") VALUES (4326,'Geographic WGS84');
+INSERT INTO "catalogue_projection" ("epsg_code", "name") VALUES (900913,'Google Mercator');
 
 COMMIT;
