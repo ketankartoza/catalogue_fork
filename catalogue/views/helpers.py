e5cee8641c348f00b918c7931467fc8a3e2dd5e0
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2011-03-26 14:24:06 +0100

    first version of html order notification email templates (ticket 32)

--- catalogue/views/helpers.py
+++ catalogue/views/helpers.py
@@ -22,8 +22,97 @@ from django.core import mail
 import zipfile
 from cStringIO import StringIO
 
-# Read from settings
+import os.path
+import re
+from email.MIMEBase import MIMEBase
+
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives, SafeMIMEMultipart
+
+# Read default notification recipients from settings
 CATALOGUE_DEFAULT_NOTIFICATION_RECIPIENTS = getattr(settings, 'CATALOGUE_DEFAULT_NOTIFICATION_RECIPIENTS', False )
+
+###########################################################
+#
+# EmailMessage subclass that makes it easy to send multipart/related
+# messages. For example, including text and HTML versions with inline images.
+#
+# courtesy of: http://www.cupcakewithsprinkles.com/html-emails-with-inline-images-in-django/
+#
+###########################################################
+
+class EmailMultiRelated(EmailMultiAlternatives):
+  """
+  A version of EmailMessage that makes it easy to send multipart/related
+  messages. For example, including text and HTML versions with inline images.
+  """
+  related_subtype = 'related'
+ 
+  def __init__(self, subject='', body='', from_email=None, to=None, bcc=None, connection=None, attachments=None, headers=None, alternatives=None):
+    # self.related_ids = []
+    self.related_attachments = []
+    return super(EmailMultiRelated, self).__init__(subject, body, from_email, to, bcc, connection, attachments, headers, alternatives)
+
+  def attach_related(self, filename=None, content=None, mimetype=None):
+    """
+    Attaches a file with the given filename and content. The filename can
+    be omitted and the mimetype is guessed, if not provided.
+
+    If the first parameter is a MIMEBase subclass it is inserted directly
+    into the resulting message attachments.
+    """
+
+    if isinstance(filename, MIMEBase):
+      assert content == mimetype == None
+      self.related_attachments.append(filename)
+    else:
+      assert content is not None
+      self.related_attachments.append((filename, content, mimetype))
+
+  def attach_related_file(self, path, mimetype=None):
+      """Attaches a file from the filesystem."""
+      filename = os.path.basename(path)
+      content = open(path, 'rb').read()
+      self.attach_related(filename, content, mimetype)
+
+  def _create_message(self, msg):
+    return self._create_attachments(self._create_related_attachments(self._create_alternatives(msg)))
+
+  def _create_alternatives(self, msg):
+    for i, (content, mimetype) in enumerate(self.alternatives):
+      if mimetype == 'text/html':
+        for filename, _, _ in self.related_attachments:
+          content = re.sub(r'(?<!cid:)%s' % re.escape(filename), 'cid:%s' % filename, content)
+          self.alternatives[i] = (content, mimetype)
+
+      return super(EmailMultiRelated, self)._create_alternatives(msg)
+
+  def _create_related_attachments(self, msg):
+    encoding = self.encoding or settings.DEFAULT_CHARSET
+    if self.related_attachments:
+      body_msg = msg
+      msg = SafeMIMEMultipart(_subtype=self.related_subtype, encoding=encoding)
+      if self.body:
+        msg.attach(body_msg)
+        for related in self.related_attachments:
+          msg.attach(self._create_related_attachment(*related))
+    return msg
+
+  def _create_related_attachment(self, filename, content, mimetype=None):
+      """
+      Convert the filename, content, mimetype triple into a MIME attachment
+      object. Adjust headers to use Content-ID where applicable.
+      Taken from http://code.djangoproject.com/ticket/4771
+      """
+      attachment = super(EmailMultiRelated, self)._create_attachment(filename, content, mimetype)
+      if filename:
+        mimetype = attachment['Content-Type']
+        del(attachment['Content-Type'])
+        del(attachment['Content-Disposition'])
+        attachment.add_header('Content-Disposition', 'inline', filename=filename)
+        attachment.add_header('Content-Type', mimetype, name=filename)
+        attachment.add_header('Content-ID', '<%s>' % filename)
+      return attachment
 
 
 ###########################################################
@@ -131,14 +220,8 @@ def notifySalesStaff(theUser, theOrderId):
   myOrder = get_object_or_404(Order,id=theOrderId)
   myRecords = SearchRecord.objects.filter(user=theUser, order=myOrder).select_related()
   myHistory = OrderStatusHistory.objects.filter(order=myOrder)
+
   myEmailSubject = 'SAC Order ' + str(myOrder.id) + ' status update (' + myOrder.order_status.name + ')'
-  myEmailMessage = 'The status for order #' +  str(myOrder.id) + ' has changed. Please visit the order page:\n'
-  myEmailMessage = myEmailMessage + 'http://' + settings.DOMAIN + '/vieworder/' + str(myOrder.id) + '/\n\n\n'
-  myTemplate = "orderEmail.txt"
-  myEmailMessage += render_to_string( myTemplate, { 'myOrder': myOrder,
-                                                    'myRecords' : myRecords,
-                                                    'myHistory' : myHistory
-                                                  })
 
   # Get a list of staff user's email addresses
   myMessagesList = [] # we will use mass_mail to prevent users seeing who other recipients are
@@ -148,28 +231,41 @@ def notifySalesStaff(theUser, theOrderId):
   for myProduct in [s.product for s in myRecords]:
     myRecipients.update(OrderNotificationRecipients.getUsersForProduct(myProduct))
 
-  # Add default
+  # Add default recipients
   if not myRecipients and CATALOGUE_DEFAULT_NOTIFICATION_RECIPIENTS:
     logging.info("Sending notice to default recipients : %s" % CATALOGUE_DEFAULT_NOTIFICATION_RECIPIENTS)
     myRecipients.update(list(CATALOGUE_DEFAULT_NOTIFICATION_RECIPIENTS))
 
   for myRecipient in myRecipients:
+    #txt email template
+    myEmailMessage_txt = render_to_string( 'mail/order.txt', { 'myOrder': myOrder,
+                                                    'myRecords' : myRecords,
+                                                    'myHistory' : myHistory,
+                                                    'myRecipient': myRecipient,
+                                                    'domain':settings.DOMAIN
+                                                  })
+    #html email template
+    myEmailMessage_html = render_to_string( 'mail/order.html', { 'myOrder': myOrder,
+                                                      'myRecords' : myRecords,
+                                                      'myHistory' : myHistory,
+                                                      'myRecipient': myRecipient,
+                                                      'domain':settings.DOMAIN
+                                                  })
     myAddress = myRecipient.email
-    myMsg = mail.EmailMultiAlternatives(myEmailSubject, myEmailMessage, 'dontreply@' + settings.DOMAIN, [myAddress])
+    myMsg = EmailMultiRelated(myEmailSubject, myEmailMessage_txt, 'dontreply@' + settings.DOMAIN, [myAddress])
     logging.info("Sending notice to : %s" % myAddress)
-    myMsg.attach_alternative('<html>test html</html>','text/html')
-    myMessagesList.append(myMsg)
 
-  #also send an email to the originator of the order
-  #We do this separately to avoid them seeing the staff cc list
-  #myClientAddress = theUser.email
-  #myMessagesList.append((myEmailSubject, myEmailMessage, settings.DEFAULT_FROM_EMAIL, [myClientAddress]))
+    #attach alternative payload - html
+    myMsg.attach_alternative(myEmailMessage_html,'text/html')
+    #add required images, as inline attachments, accesed by 'name' in templates
+    myMsg.attach_related_file(os.path.join(settings.MEDIA_ROOT,'images','sac_header.jpg'))
+    #add message
+    myMessagesList.append(myMsg)
 
   logging.info("Sending messages: \n%s" % myMessagesList)
   # initiate email connection, and send messages in bulk
   myEmailConnection = mail.get_connection()
   myEmailConnection.send_messages(myMessagesList)
-  myEmailConnection.close()
   return
 
 ###########################################################
