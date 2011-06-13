3c4ad58d12f70402b85fd3ee1108404ee13d9132
Tim Sutton <tim@linfiniti.com>
2011-06-13 17:11:58 +0200

    Fix sql migrator - treat orbit as a separate transaction

--- sql/migrations/102-projections-insert.sql
+++ sql/migrations/102-projections-insert.sql
@@ -1,6 +1,8 @@
 BEGIN;
-
 INSERT INTO "catalogue_projection" ("epsg_code", "name") VALUES (0,'ORBIT');
+COMMIT;
+BEGIN;
+
 INSERT INTO "catalogue_projection" ("epsg_code", "name") VALUES (4326,'Geographic WGS84');
 INSERT INTO "catalogue_projection" ("epsg_code", "name") VALUES (900913,'Google Mercator');
 
