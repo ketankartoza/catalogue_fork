c7ebf3a82d4936d16d0a147ccff90662fb4c4077
Tim Sutton <tim@linfiniti.com>
2011-06-15 00:08:36 +0200

    Updated box_tag so that you can pass classes to it (had to not use simple tag anymore for this). Other updates to theming and layout of front page. Update css so boxes dont have gradient bg.

--- catalogue/templatetags/boxtag.py
+++ catalogue/templatetags/boxtag.py
@@ -1,26 +1,53 @@
-## Original code from Tim's SABIO project and copyright is retained by the original author #
 from django import template
-     
+# python logging support to django logging middleware
+import logging
 register = template.Library()     
-@register.simple_tag
-def box_start( id ):
-  myString = '''
-    <div class="ui-widget append-bottom">
-      <div class="ui-helper-reset ui-widget-content ui-state-highlight ui-corner-all"  style="padding: 10px; min-height: 100px;" id="%s" >''' % id
-  if heading:
-    myString = myString + '''
-    <p><span class="ui-icon ui-icon-alert" style="float: left; margin-right: .3em;"></span>
-    ''' 
-    + str(heading) + '''
-    </p>
-    '''
-  myString = myString + '''<div>'''
-  return myString
-    
+
+class BoxStartNode(template.Node):
+  def __init__(self, div_id, div_classes=None):
+    self.div_id = div_id.replace("\"","").replace("'","")
+    self.div_classes=div_classes
+    if self.div_classes:
+      self.div_classes = self.div_classes.replace("\"","").replace("'","")
+    logging.info("BoxStartNode init - self.div_id : %s" % self.div_id)
+    logging.info("BoxStartNode init - self.div_classes : %s" % self.div_classes)
+  def render(self, context):
+    myClasses = "ui-helper-reset ui-widget-content ui-state-highlight ui-corner-all"
+    if self.div_classes:
+      myClasses = "%s %s" % ( myClasses, self.div_classes )
+    myString = '''
+      <div class="ui-widget append-bottom">
+        <div class="%s"  style="padding: 10px; min-height: 100px;" id="%s" >''' % ( myClasses, self.div_id )
+    return myString
+
+@register.tag     
+def box_start(parser, token):
+  # if the tag is like this {% box_start "foo-id" "foo-class barclass" %}
+  # or if the tag is like this {% box_start foo-id "foo-class barclass" %}
+  # token.split_contents would return 3 elements, box_start (the tag name), 
+  # "foo-id" and "foo-class...."
+  # In our tag, the classes (css classes that will be added to the outer box)
+  # is optional. Quotes are mandatory on the classes list param and optional
+  # for the div_id
+  myArgCount = len(token.split_contents())
+  if myArgCount == 2:
+    # split_contents() knows not to split quoted strings.
+    tag_name, div_id = token.split_contents()
+    return BoxStartNode(div_id)
+  elif myArgCount == 3:
+    # split_contents() knows not to split quoted strings.
+    tag_name, div_id, div_classes = token.split_contents()
+    # check div_classes is quoted and quoted using matching double or single quotes
+    if not (div_classes[0] == div_classes[-1] and div_classes[0] in ('"', "'")):
+      raise template.TemplateSyntaxError("%r tag's classes list argument should be in quotes" % tag_name)
+    return BoxStartNode(div_id, div_classes)
+  else: 
+    raise template.TemplateSyntaxError("%r tag requires either one or two args." % token.contents.split()[0])
+
+
 @register.simple_tag
 def box_end( ):
   return '''
-        </div>
       </div>
     </div>
   '''
