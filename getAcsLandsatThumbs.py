dea474cd2182f603a681f153d86ed3444e641f00
Tim Sutton <tim@linfiniti.com>
2011-07-05 16:52:09 +0200

    Fixes to acs fetcher

--- getAcsLandsatThumbs.py
+++ getAcsLandsatThumbs.py
@@ -1,9 +1,12 @@
 from catalogue.informix import Informix
-myFile = file( "lastlandsat.txt", "rwt" )
+myFile = file( "lastlandsat.txt", "rt" )
 myLastRecord = myFile.read()
 myFile.close()
 myI = Informix()
-myQuery = 'select FIRST %i * from t_landsat_frame where localization_id > %s' % ( 20000, myLastRecord )
+myQuery = 'select count(*) as count from t_landsat_frame'
+myRows = myI.runQuery( myQuery )
+print "Total number of landsat recs in ACS %s" % myRows[0]['count']
+myQuery = 'select FIRST %i * from t_landsat_frame where localization_id > %s' % ( 50000, myLastRecord )
 myRows = myI.runQuery( myQuery )
 myErrorCount=0
 myOkCount=0
@@ -11,11 +14,12 @@ for myRow in myRows:
   try:
     myLastRecord = myRow['localization_id']
     myI.thumbForLocalization( myRow['localization_id'] )
-    myFile = file( "lastlandsat.txt", "rwt" )
-    myFile.write( myLastRecord )
+    myFile = file( "lastlandsat.txt", "wt" )
+    myFile.write( str(myLastRecord) )
     myFile.close()
     myOkCount+=1
   except:
     myErrorCount+=1
-    continue
+    raise
+myI.cleanup()
 print "Completed: %s imports ok, %s errors" % (myOkCount, myErrorCount)
