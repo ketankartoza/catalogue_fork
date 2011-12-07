5780879e8e29ea35192ba463784a3019426c24c3
Tim Sutton <tim@linfiniti.com>
2011-12-07 12:22:03 +0200

    Added shell and runserver scripts

---
+++ runserver.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "Run $0 -d to run standard runserver in debugger mode"
+source ../python/bin/activate
+#python manage.py collectstatic --noinput
+
+if [ $# -ne 1 ] 
+then
+  echo "Running using runserver_plus"
+  python manage.py runserver_plus
+else
+  echo "Running using runserver --pdb"
+  echo "Note: does not currently work with django 1.3 static_files app"
+  #pdb option below requires installation of django-pdb and 
+  #will break at the start of each view
+  # alternatively add 
+  # import pdb; pdb.set_trace()
+  # anywhere in the source to force a breakpoint
+  # doesnt work with runserver_plus
+  python manage.py runserver
+  #--pdb
+fi
