45e29030a1d07431ac52221ceebf021b2d0f7f57
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-06-11 15:44:34 +0200

    Added SQL scripts for Story 126
    Story #126 reviewing

---
+++ sql/fixes/story126_owner_information_2.sql
@@ -0,0 +1,8 @@
+BEGIN;
+INSERT INTO catalogue_institution (name,address1,address2,address3,post_code) VALUES
+('INPE & CAST','','','',''),
+('ESA','','','',''),
+('USGS','','','',''),
+('CONAE','','','',''),
+('NOAA','','','','');
+COMMIT;
