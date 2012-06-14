28cce7c84a238e848dcaeb827880a38ad3ffdbf7
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-06-14 15:41:36 +0200

    Added RequestFactory for faking HTTP requests to test_utils
    Built-in as of Django 1.3, https://docs.djangoproject.com/en/dev/topics/testing/#the-request-factory
    
    Snippet code: http://djangosnippets.org/snippets/963/

--- catalogue/tests/test_utils.py
+++ catalogue/tests/test_utils.py
@@ -17,9 +17,53 @@ __version__ = '0.1'
 __date__ = '13/06/2012'
 __copyright__ = 'South African National Space Agency'
 
+from django.test import Client
+from django.core.handlers.wsgi import WSGIRequest
+
 
 def simpleMessage(theResult, theExpectedResult):
     """Format simple assert message"""
 
     return 'Got: %s \nExpected: %s ' % (theResult,
         theExpectedResult)
+
+
+#
+# IMPORTANT: this is built-in class in Django 1.3
+# https://docs.djangoproject.com/en/dev/topics/testing/#the-request-factory
+#
+class RequestFactory(Client):
+    """
+    Class that lets you create mock Request objects for use in testing.
+
+    Usage:
+
+    rf = RequestFactory()
+    get_request = rf.get('/hello/')
+    post_request = rf.post('/submit/', {'foo': 'bar'})
+
+    This class re-uses the django.test.client.Client interface, docs here:
+    http://www.djangoproject.com/documentation/testing/#the-test-client
+
+    Once you have a request object you can pass it to any view function,
+    just as if that view had been hooked up using a URLconf.
+
+    """
+    def request(self, **request):
+        """
+        Similar to parent class, but returns the request object as soon as it
+        has created it.
+        """
+        environ = {
+            'HTTP_COOKIE': self.cookies,
+            'PATH_INFO': '/',
+            'QUERY_STRING': '',
+            'REQUEST_METHOD': 'GET',
+            'SCRIPT_NAME': '',
+            'SERVER_NAME': 'testserver',
+            'SERVER_PORT': 80,
+            'SERVER_PROTOCOL': 'HTTP/1.1',
+        }
+        environ.update(self.defaults)
+        environ.update(request)
+        return WSGIRequest(environ)
