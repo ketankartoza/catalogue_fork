11a6ed2d292eca1e9fe38ab8b04ff956dd25eb8f
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-06-15 11:34:16 +0200

    Updated/fixed testing fixtures
    Story #82

--- sql/sample_products.sql
+++ sql/sample_products.sql
@@ -11,14 +11,23 @@ drop table sample_opticalproduct;
 create table sample_opticalproduct AS 
 SELECT * from catalogue_opticalproduct
 ORDER BY random()
-LIMIT 1000;
+LIMIT 950;
+
+-- create 50 random radarproducts 
+DROP table sample_radarproduct;
+create table sample_radarproduct AS 
+SELECT * from catalogue_radarproduct
+ORDER BY random()
+LIMIT 50;
 
 -- for that 1000 products create parent tables
 drop table sample_genericsensorproduct;
 create table sample_genericsensorproduct AS
 select b.* from sample_opticalproduct a inner join catalogue_genericsensorproduct b
-ON  a.genericsensorproduct_ptr_id = b.genericimageryproduct_ptr_id;
-
+ON a.genericsensorproduct_ptr_id = b.genericimageryproduct_ptr_id
+UNION ALL
+select b.* from sample_radarproduct a inner join catalogue_genericsensorproduct b
+ON b.genericimageryproduct_ptr_id = a.genericsensorproduct_ptr_id;
 
 drop table sample_genericimageryproduct;
 create table sample_genericimageryproduct AS
@@ -29,3 +38,4 @@ drop table sample_genericproduct;
 create table sample_genericproduct AS
 select b.* from sample_genericimageryproduct a inner join catalogue_genericproduct b
 ON  a.genericproduct_ptr_id = b.id;
+
