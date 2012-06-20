35ec0d24695b1be82542c38f1238eb416342b705
Tim Sutton <tim@linfiniti.com>
2012-06-20 14:23:23 +0200

    Added script to run all sql fix scripts in sequence

---
+++ sql/fixes/run_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]
+then
+  echo "Usage: $(basename $0) database_name"
+  exit $E_BADARGS
+fi
+
+for script in $(ls *fix.sh|sort)
+do
+    echo "Executing script:", $script
+    bash $script $1
+done
