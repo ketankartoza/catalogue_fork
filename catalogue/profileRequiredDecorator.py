d483e652ae68b6a742a67761343d07e70d0ebace
Tim Sutton <tim@linfiniti.com>
2012-06-22 15:11:34 +0200

    PEP8 compliance for settings template

--- catalogue/profileRequiredDecorator.py
+++ catalogue/profileRequiredDecorator.py
@@ -14,53 +14,61 @@
    once they have completed their details.
    """
 
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
-# python logging support to django logging middleware
 import logging
 # for error logging
 import traceback
-
 from functools import wraps
 
-def requireProfile( theView ):
-    """Note this will not check the validity of the profile, only its existance.
-    It relies on the user profile form to ensure sufficient details are collected
+
+def requireProfile(theView):
+    """Note this will not check the validity of the profile, only its
+       existance. It relies on the user profile form to ensure sufficient
+       details are collected
     """
-    def decorator( theFunction ):
-        def inner_decorator( theRequest, *args, **kwargs):
+    def decorator(theFunction):
+        def inner_decorator(theRequest, *args, **kwargs):
             try:
                 myProfile = theRequest.user.get_profile()
-                if myProfile and checkProfile( myProfile ):
+                if myProfile and checkProfile(myProfile):
                     logging.info('User Profile is populated')
-                    logging.debug('User Profile is populated')
-                    return theFunction( theRequest, *args, **kwargs )
+                    return theFunction(theRequest, *args, **kwargs)
                 else:
                     logging.info('User Profile is NOT populated - redirecting')
-                    logging.debug('User Profile is NOT populated - redirecting')
                     if theRequest.is_ajax():
-                        #we need to tell the client to do the redirection in this case...
-                        myJscriptRelocate = """<script>window.location.replace("/accounts/profile/edit/personal/?next=/%s/");</script>""" % theView
-                        return HttpResponse( myJscriptRelocate, mimetype='application/javascript' )
+                        #we need to tell the client to do the redirection
+                        # in this case...
+                        myJscriptRelocate = """
+                             <script>
+                             window.location.replace(
+                                 "/accounts/profile/edit/personal/?next=/%s/");
+                             </script>""" % theView
+                        return HttpResponse(myJscriptRelocate,
+                                            mimetype='application/javascript')
                     else:
-                        return HttpResponseRedirect( "/accounts/profile/edit/personal/?next=/%s/" % theView )
+                        return HttpResponseRedirect(
+                             "/accounts/profile/edit/personal/?next=/%s/" %
+                             theView)
             except:
                 logging.info('User Profile exception - redirecting')
                 logging.debug('User Profile exception - redirecting')
                 logging.debug(traceback.format_exc())
-                return HttpResponseRedirect( "/accounts/profile/edit/personal/?next=/%s/" % theView )
-        return wraps( theFunction )(inner_decorator)
+                return HttpResponseRedirect(
+                        "/accounts/profile/edit/personal/?next=/%s/" % theView)
+        return wraps(theFunction)(inner_decorator)
     return decorator
 
-def checkProfile( theProfile ):
-    """Does basic checking that required fields are not just populated with zero length strings"""
-    if not theProfile.firstname \
-        or not theProfile.surname \
-        or not theProfile.address1 \
-        or not theProfile.address2 \
-        or not theProfile.post_code \
-        or not theProfile.organisation \
-        or not theProfile.contact_no:
+
+def checkProfile(theProfile):
+    """Does basic checking that required fields are not just populated with
+      zero length strings."""
+    if not (theProfile.firstname
+        or theProfile.surname
+        or theProfile.address1
+        or theProfile.address2
+        or theProfile.post_code
+        or theProfile.organisation
+        or theProfile.contact_no):
         return False
     else:
         return True
