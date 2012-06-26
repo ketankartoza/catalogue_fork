ce404020d5badea32777c90b6cf3b26f9e0528a5
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-06-26 17:43:40 +0200

    Added unittests for GenericProduct model
    Story #218 review

--- catalogue/tests/__init__.py
+++ catalogue/tests/__init__.py
@@ -25,6 +25,7 @@ from topic_model import TopicCRUD_Test
 from placeType_model import PlaceTypeCRUD_Test
 from place_model import PlaceCRUD_Test
 from unit_model import UnitCRUD_Test
+from genericproduct_model import GenericProductCRUD_Test
 from featurereaders_return import FeatureReaders_Test
 from searcher_object import SearcherObject_Test
 from search_bandcount import SearchBandCount_Test
