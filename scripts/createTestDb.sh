b4e790c9a2c5603b74185cc65ce76401ed23d3b9
Tim Sutton <tim@linfiniti.com>
2012-07-09 14:48:54 +0200

    Fix test db loader and email notice unit tests so that the test db contains a valid order, userprofile etc.

--- scripts/createTestDb.sh
+++ scripts/createTestDb.sh
@@ -14,6 +14,10 @@ python manage.py testserver \
   catalogue/fixtures/test_license.json\
   catalogue/fixtures/test_projection\
   catalogue/fixtures/test_quality\
+  catalogue/fixtures/test_order.json\
+  catalogue/fixtures/test_searchrecord.json\
+  catalogue/fixtures/test_sacuserprofile.json\
+  catalogue/fixtures/test_orderstatus.json\
   catalogue/fixtures/test_creatingsoftware\
   catalogue/fixtures/test_genericproduct.json\
   catalogue/fixtures/test_genericimageryproduct.json\
@@ -21,10 +25,11 @@ python manage.py testserver \
   catalogue/fixtures/test_opticalproduct.json\
   catalogue/fixtures/test_radarproduct.json\
   catalogue/fixtures/test_datum.json\
+  catalogue/fixtures/test_marketsector.json\
   catalogue/fixtures/test_deliverymethod.json\
   catalogue/fixtures/test_fileformat.json\
   catalogue/fixtures/test_resamplingmethod.json
 
-dropdb sac-testbed
-psql -c 'alter database "sac-unittest" rename to "sac-testbed;"' template1
+dropdb 'sac-testbed'
+psql -c 'alter database "sac-unittest" rename to "sac-testbed"' template1
 deactivate
