cb72c4f9c382eafea3b2daf747f5ddc96941b001
Linda Kleyn <lkleyn@sansa.org.za>
2012-07-10 16:23:43 +0200

    Updated spot importer to include all years

--- import-spot.sh
+++ import-spot.sh
@@ -12,20 +12,22 @@ python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPO
 echo "2003"
 python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2003.shp'
 echo "2004"
-#python manage.py spot_harvest -v 1 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2004.shp'
+python manage.py spot_harvest -v 1 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2004.shp'
 echo "2005"
 python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2005.shp'
 echo "2006"
-#python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2006.shp'
+python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2006.shp'
 echo "2007"
-#python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2007.shp'
+python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2007.shp'
 echo "2008"
-#python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2008.shp'
+python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2008.shp'
 echo "2009"
-#python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2009.shp'
+python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2009.shp'
 echo "2010"
-#python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2010.shp'
+python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2010.shp'
 echo "2011"
 python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2011.shp'
+echo "2012"
+python manage.py spot_harvest -v 0 -f '/mnt/cataloguestorage2/gisdata/africa/SPOT_Coverage/Africa_2012.shp'
 #python manage.py spot_harvest -v 0 -f '/home/timlinux/gisdata/Africa/SPOT_Coverage/SPOT_Shape_Africa_2011/Africa_2011.shp'
 deactivate
