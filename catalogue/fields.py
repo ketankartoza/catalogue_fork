ad7e933b75e3f3f40388746308a606c09df04f3f
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-07-12 12:20:31 +0200

    Improved wording of error validation message for Row&Path searches
    Story #302 reviewing
    Story #301 reviewing

--- catalogue/fields.py
+++ catalogue/fields.py
@@ -37,7 +37,7 @@ class IntegersCSVIntervalsField(forms.RegexField):
         for value in IntegersCSVIntervalsField.to_tuple(values):
             if len(value) == 2:
                 if not value[1] > value[0]:
-                    raise forms.ValidationError, 'The range values are not correct: %d %d' % value
+                    raise forms.ValidationError, 'First value in range (%d) must be lower then second (%d)' % value
 
 
 class NoValidationChoiceField(forms.ChoiceField):
