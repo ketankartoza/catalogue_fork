184df9f1473cbf0b83cf1042b53a25c17675c6e4
Tim Sutton <tim@linfiniti.com>
2012-07-26 15:29:48 +0200

    SQL update scripts from story #354 - remove bogus acq code related products

---
+++ sql/fixes/iteration1/story354_fix_remove_bogus_acquisition_id_spot_products.sql
@@ -0,0 +1,11 @@
+BEGIN;
+-- 434375 records - keep ids of products to be deleted in a temp table
+select genericimageryproduct_ptr_id as id into tmp from catalogue_genericsensorproduct where acquisition_mode_id in (
+select id from catalogue_acquisitionmode where sensor_type_id  in ( select id from catalogue_sensortype where mission_sensor_id = 6));
+delete from catalogue_opticalproduct where genericsensorproduct_ptr_id in (select id from tmp);
+delete from catalogue_genericsensorproduct where genericimageryproduct_ptr_id in (select id from tmp);
+delete from catalogue_genericimageryproduct where genericproduct_ptr_id in (select id from tmp);
+delete from catalogue_searchrecord where product_id in (select id from tmp);
+delete from catalogue_genericproduct where id in (select id from tmp);
+drop table tmp;
+COMMIT;
