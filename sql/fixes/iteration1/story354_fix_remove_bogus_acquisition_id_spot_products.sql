a7a7ffd0d990a262605aa1b05bd40f09c69cd4a5
Tim Sutton <tim@linfiniti.com>
2012-07-31 09:33:16 +0200

    Added some missing delete commands for story #354

--- sql/fixes/iteration1/story354_fix_remove_bogus_acquisition_id_spot_products.sql
+++ sql/fixes/iteration1/story354_fix_remove_bogus_acquisition_id_spot_products.sql
@@ -7,5 +7,8 @@ delete from catalogue_genericsensorproduct where genericimageryproduct_ptr_id in
 delete from catalogue_genericimageryproduct where genericproduct_ptr_id in (select id from tmp);
 delete from catalogue_searchrecord where product_id in (select id from tmp);
 delete from catalogue_genericproduct where id in (select id from tmp);
+delete from catalogue_acquisitionmode where sensor_type_id  in ( select id from catalogue_sensortype where mission_sensor_id = 6));
+delete from catalogue_sensortype where mission_sensor_id = 6;
+delete from catalogue_missionsensor where id = 6;
 drop table tmp;
 COMMIT;
