0c48494337ff83bccf9eb19ed481c3430a0a3352
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-08-16 15:59:39 +0200

    Updated templatetags/* to conform coding standards
    Story #373 reviewing

--- catalogue/templatetags/boxtag.py
+++ catalogue/templatetags/boxtag.py
@@ -1,24 +1,50 @@
+"""
+SANSA-EO Catalogue - Box element template tags
+
+Contact : lkleyn@sansa.org.za
+
+.. note:: This program is the property of the South African National Space
+   Agency (SANSA) and may not be redistributed without expresse permission.
+   This program may include code which is the intellectual property of
+   Linfiniti Consulting CC. Linfiniti grants SANSA perpetual, non-transferrable
+   license to use any code contained herein which is the intellectual property
+   of Linfiniti Consulting CC.
+
+"""
+
+__author__ = 'tim@linfiniti.com'
+__version__ = '0.1'
+__date__ = '16/08/2012'
+__copyright__ = 'South African National Space Agency'
+
 from django import template
-# python logging support to django logging middleware
 import logging
 register = template.Library()
 
+
 class BoxStartNode(template.Node):
     def __init__(self, div_id, div_classes=None):
-        self.div_id = div_id.replace("\"","").replace("'","")
-        self.div_classes=div_classes
+        self.div_id = div_id.replace('\"', '').replace('\'', '')
+        self.div_classes = div_classes
         if self.div_classes:
-            self.div_classes = self.div_classes.replace("\"","").replace("'","")
+            self.div_classes = (
+                self.div_classes.replace('\"', '').replace('\'', ''))
         #logging.info("BoxStartNode init - self.div_id : %s" % self.div_id)
-        #logging.info("BoxStartNode init - self.div_classes : %s" % self.div_classes)
+        #logging.info("BoxStartNode init - self.div_classes : %s" % (
+        #    self.div_classes,))
+
     def render(self, context):
-        myClasses = "ui-helper-reset ui-widget-content ui-state-highlight ui-corner-all"
+        myClasses = (
+            'ui-helper-reset ui-widget-content ui-state-highlight '
+            'ui-corner-all')
         if self.div_classes:
-            myClasses = "%s %s" % ( myClasses, self.div_classes )
+            myClasses = "%s %s" % (myClasses, self.div_classes)
         myString = '''
           <div class="ui-widget append-bottom">
-            <div class="%s"  style="min-height: 100px;" id="%s" >''' % ( myClasses, self.div_id )
+            <div class="%s"  style="min-height: 100px;" id="%s" >''' % (
+            myClasses, self.div_id)
         return myString
+
 
 @register.tag
 def box_start(parser, token):
@@ -37,16 +63,22 @@ def box_start(parser, token):
     elif myArgCount == 3:
         # split_contents() knows not to split quoted strings.
         tag_name, div_id, div_classes = token.split_contents()
-        # check div_classes is quoted and quoted using matching double or single quotes
-        if not (div_classes[0] == div_classes[-1] and div_classes[0] in ('"', "'")):
-            raise template.TemplateSyntaxError("%r tag's classes list argument should be in quotes" % tag_name)
+        # check div_classes is quoted and quoted using matching double or
+        # single quotes
+        if not (div_classes[0] == div_classes[-1] and
+                div_classes[0] in ('"', "'")):
+            raise template.TemplateSyntaxError(
+                '%r tag\'s classes list argument should be in quotes' % (
+                    tag_name,))
         return BoxStartNode(div_id, div_classes)
     else:
-        raise template.TemplateSyntaxError("%r tag requires either one or two args." % token.contents.split()[0])
+        raise template.TemplateSyntaxError(
+            '%r tag requires either one or two args.' % (
+                token.contents.split()[0],))
 
 
 @register.simple_tag
-def box_end( ):
+def box_end():
     return '''
         </div>
       </div>
@@ -54,7 +86,7 @@ def box_end( ):
 
 
 @register.simple_tag
-def error_message_start( heading ):
+def error_message_start(heading):
     return '''
       <div class="ui-widget">
         <div class="ui-state-error ui-corner-all" style="padding: 0 .7em;">
@@ -64,8 +96,9 @@ def error_message_start( heading ):
           <div>
           '''
 
+
 @register.simple_tag
-def error_message_end( ):
+def error_message_end():
     return '''
           </div>
         </div>
