8e3b873e74047c763b799ca52a3bb9345e928886
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-08-17 09:23:21 +0200

    Updated views/geoiputils.py to conform coding standards
    Story #374 reviewing

--- catalogue/views/geoiputils.py
+++ catalogue/views/geoiputils.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render_to_response
+"""
+SANSA-EO Catalogue - GeoIPUtils helper class
+
+Contact : lkleyn@sansa.org.za
+
+.. note:: This program is the property of the South African National Space
+   Agency (SANSA) and may not be redistributed without expresse permission.
+   This program may include code which is the intellectual property of
+   Linfiniti Consulting CC. Linfiniti grants SANSA perpetual, non-transferrable
+   license to use any code contained herein which is the intellectual property
+   of Linfiniti Consulting CC.
+
+"""
+
+__author__ = 'tim@linfiniti.com'
+__version__ = '0.1'
+__date__ = '17/08/2012'
+__copyright__ = 'South African National Space Agency'
+
+
 from django.contrib.gis.utils.geoip import GeoIP
-import re #regex support
+import re  # regex support
 import urllib2
 
 # python logging support to django logging middleware
 import logging
 import traceback
+
 
 class GeoIpUtils:
     """A class for resolving lat/long from and IP address"""
@@ -17,7 +37,7 @@ class GeoIpUtils:
         """Destructor"""
         return
 
-    def getMyIp(self,request):
+    def getMyIp(self, request):
         """
         Fetch an IP when request.META returns localhost
         """
@@ -29,20 +49,20 @@ class GeoIpUtils:
                 remote_ip = matcher.search(checkip).group()
             except:
                 remote_ip = ''
-        logging.info( "Remote ip is: " + remote_ip )
+        logging.info('Remote ip is: ' + remote_ip)
         return remote_ip
 
-    def getMyLatLong(self,request):
+    def getMyLatLong(self, request):
         g = GeoIP()
         remote_ip = self.getMyIp(request)
         try:
             remote_location = g.city(remote_ip)
             if remote_location:
-                logging.info( remote_location )
+                logging.info(remote_location)
                 return remote_location
-            else: # ip cannot be found
-                logging.info( "IP could not be looked up :-(" )
+            else:  # ip cannot be found
+                logging.info('IP could not be looked up :-(')
                 return None
         except Exception, e:
-            logging.info( traceback.format_exc() )
+            logging.info(traceback.format_exc())
             return None
