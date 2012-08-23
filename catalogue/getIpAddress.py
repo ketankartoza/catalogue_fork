4f7ff028b83f48676ea82ea32164793352d31b7a
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-08-23 19:22:50 +0200

    Updated catalogue/getIpAddress.py to conform coding standards
    Story #397 reviewing

--- catalogue/getIpAddress.py
+++ catalogue/getIpAddress.py
@@ -1,3 +1,22 @@
+"""
+SANSA-EO Catalogue - Helper utilities
+
+Contact : lkleyn@sansa.org.za
+
+.. note:: This program is the property of the South African National Space
+   Agency (SANSA) and may not be redistributed without expresse permission.
+   This program may include code which is the intellectual property of
+   Linfiniti Consulting CC. Linfiniti grants SANSA perpetual, non-transferrable
+   license to use any code contained herein which is the intellectual property
+   of Linfiniti Consulting CC.
+
+"""
+
+__author__ = 'tim@linfiniti.com'
+__version__ = '0.1'
+__date__ = '01/01/2011'
+__copyright__ = 'South African National Space Agency'
+
 #
 # General Helper Utilities
 #
