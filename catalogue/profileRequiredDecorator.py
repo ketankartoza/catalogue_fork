2e2ebcf1d09a70f0512100869e83f202859b28c6
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-08-24 13:48:17 +0200

    Updated catalogue/profileRequiredDecorator.py to conform coding standards
    Story #400 reviewing

--- catalogue/profileRequiredDecorator.py
+++ catalogue/profileRequiredDecorator.py
@@ -1,18 +1,22 @@
-"""This is a decorator that when used will ensure that the user
-   has filled in all their personal details before being allowed
-   to continue.
+"""
+SANSA-EO Catalogue - Decorator used for checking if user filled all profile
+                     attributes
 
-   example useage in your view:
+Contact : lkleyn@sansa.org.za
 
-   from catalogue.profileRequiredDecorator import requireProfile
+.. note:: This program is the property of the South African National Space
+   Agency (SANSA) and may not be redistributed without expresse permission.
+   This program may include code which is the intellectual property of
+   Linfiniti Consulting CC. Linfiniti grants SANSA perpetual, non-transferrable
+   license to use any code contained herein which is the intellectual property
+   of Linfiniti Consulting CC.
 
-   @requireProfile('demo')
-   def demo(request):
-       return {}
+"""
 
-   The userprofile system will then redirect the user back to this page
-   once they have completed their details.
-   """
+__author__ = 'tim@linfiniti.com'
+__version__ = '0.1'
+__date__ = '01/01/2011'
+__copyright__ = 'South African National Space Agency'
 
 from django.http import HttpResponse, HttpResponseRedirect
 import logging
@@ -22,9 +26,25 @@ from functools import wraps
 
 
 def requireProfile(theView):
-    """Note this will not check the validity of the profile, only its
-       existance. It relies on the user profile form to ensure sufficient
-       details are collected
+    """
+    This is a decorator that when used will ensure that the user
+    has filled in all their personal details before being allowed
+    to continue.
+
+    example useage in your view:
+
+    from catalogue.profileRequiredDecorator import requireProfile
+
+    @requireProfile('demo')
+    def demo(request):
+        return {}
+
+    The userprofile system will then redirect the user back to this page
+    once they have completed their details.
+
+    Note this will not check the validity of the profile, only its
+        existance. It relies on the user profile form to ensure sufficient
+        details are collected
     """
     def decorator(theFunction):
         def inner_decorator(theRequest, *args, **kwargs):
@@ -39,22 +59,23 @@ def requireProfile(theView):
                         #we need to tell the client to do the redirection
                         # in this case...
                         myJscriptRelocate = """
-                             <script>
-                             window.location.replace(
-                                 "/accounts/profile/edit/personal/?next=/%s/");
-                             </script>""" % theView
-                        return HttpResponse(myJscriptRelocate,
-                                            mimetype='application/javascript')
+                            <script>
+                            window.location.replace(
+                                "/accounts/profile/edit/personal/?next=/%s/");
+                            </script>""" % theView
+                        return HttpResponse(
+                            myJscriptRelocate,
+                            mimetype='application/javascript')
                     else:
                         return HttpResponseRedirect(
-                             "/accounts/profile/edit/personal/?next=/%s/" %
-                             theView)
+                            "/accounts/profile/edit/personal/?next=/%s/" %
+                            theView)
             except:
                 logging.info('User Profile exception - redirecting')
                 logging.debug('User Profile exception - redirecting')
                 logging.debug(traceback.format_exc())
                 return HttpResponseRedirect(
-                        "/accounts/profile/edit/personal/?next=/%s/" % theView)
+                    '/accounts/profile/edit/personal/?next=/%s/' % theView)
         return wraps(theFunction)(inner_decorator)
     return decorator
 
@@ -63,12 +84,12 @@ def checkProfile(theProfile):
     """Does basic checking that required fields are not just populated with
       zero length strings."""
     if not (theProfile.firstname
-        or theProfile.surname
-        or theProfile.address1
-        or theProfile.address2
-        or theProfile.post_code
-        or theProfile.organisation
-        or theProfile.contact_no):
+            or theProfile.surname
+            or theProfile.address1
+            or theProfile.address2
+            or theProfile.post_code
+            or theProfile.organisation
+            or theProfile.contact_no):
         return False
     else:
         return True
