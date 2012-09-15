eeed9f51a583c3a12cf8727aa39c318ab59f14c3
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2012-09-15 15:04:25 +0200

    Implemented Sansa-datepicker widget

--- media/js/widget.sansa-datepicker.js
+++ media/js/widget.sansa-datepicker.js
@@ -1,90 +1,76 @@
 $.widget( "linfinity.sansa_datepicker", {
     // default options
     options: {
-        date_focus: 'start'
+        dateFormat: 'd-m-yy'
     },
 
-/*
-        $("#%(id)s-widget").datepicker({
-        minDate: new Date(1970, 1, 1),
-        yearRange: "1972:+1",
-        dateFormat: 'dd-m-yy',
-        maxDate: '+1Y',
-        changeMonth: true,
-        changeYear: true,
-        onChangeMonthYear: function(year, month, inst) {
-            debugger;
-            myId = "%(id)s";
-            if ( myId.substring(0,8) == "id_start")
-            {
-            $("#%(id)s-widget").datepicker("setDate", "01-" + month + "-" + year);
-            }
-            else if ( myId.substring(0,6) == "id_end")
-            {
-            //calculate the last day of the month
-            //see http://javascript.about.com/library/bllday.htm
-            var myLastDay = new Date(year, month, 0).getDate();
-            $("#%(id)s-widget").datepicker("setDate",
-                myLastDay + "-" + month + "-" + year);
-            }
-            // returns a js date object
-            var myDate = $("#%(id)s-widget").datepicker("getDate");
-            var myDay = myDate.getDate();
-            if ( myDay < 10 ) //zero pad the day
-            {
-            myDay = "0" + myDay;
-            }
-            //+1 below because js months start at 0
-            var myMonth = myDate.getMonth() + 1;
-            if ( myMonth < 10 ) //zero pad the day
-            {
-            myMonth = "0" + myMonth;
-            }
-            var myYear  = myDate.getFullYear();
-            var myTextDate = myDay + "-" + myMonth + "-" + myYear;
-            $('#%(id)s').val( myTextDate );
-        },
-        onSelect: function( theDate, inst)
-            {
-            check_search_dates();
-            $('#%(id)s').val( theDate );
-            }
-        %(defaultDatePropery)s
-        });
-    $("#%(id)s-widget").datepicker("setDate", "%(defaultDate)s");
-*/
     // the constructor
     _create: function() {
         var self=this;
 
-        //update options from html5data
+        this.input_element = this.element.find('input');
+
+        //update date_focus options from html5data
         var mydate_focus = this.element.data('date_focus');
         if (mydate_focus) {
             this.options.date_focus = mydate_focus;
         }
 
-        var myDatePicker = this.element.datepicker({
-        minDate: new Date(1970, 1, 1),
-        yearRange: "1972:+1",
-        dateFormat: 'dd-m-yy',
-        maxDate: '+1Y',
-        changeMonth: true,
-        changeYear: true,
+        //set default_date
+        this.current_date = new Date();
 
-        onChangeMonthYear: function(year, month, inst) {
-            if (self.options.date_focus === 'start') {
-                myDatePicker.datepicker("setDate", "01-" + month + "-" + year);
-            } else if (self.options.date_focus === 'end') {
-                //calculate the last day of the month
-                //see http://javascript.about.com/library/bllday.htm
-                var myLastDay = new Date(year, month, 0).getDate();
-                myDatePicker.datepicker("setDate", myLastDay + "-" + month + "-" + year);
-            } else {
-                //if date_focus is not set
+        this.element.datepicker({
+            minDate: new Date(1970, 1, 1),
+            yearRange: "1972:+1",
+            dateFormat: this.options.dateFormat,
+            maxDate: '+1Y',
+            changeMonth: true,
+            changeYear: true,
+
+            onChangeMonthYear: function(year, month, inst) {
+                var newDate = null;
+                if (self.options.date_focus === 'start') {
+                    self._set_date(self._get_first_day_of_month(year, month));
+                } else if (self.options.date_focus === 'end') {
+                    self._set_date(self._get_last_day_of_month(year, month));
+                } else {
+                    //if date_focus is not set
+                }
             }
-        }
         });
 
-    }
+        //update widget
+        if (this.options.date_focus === 'start') {
+            self._set_date(self._get_first_day_of_month(this.current_date.getFullYear(), this.current_date.getMonth()+1));
+        } else if (this.options.date_focus === 'end') {
+            self._set_date(self._get_last_day_of_month(this.current_date.getFullYear(), this.current_date.getMonth()+1));
+        } else {
+            //set TODAY as current date
+            this._set_date(this.current_date);
+        }
+    },
 
+    _set_date: function(theDate) {
+        this.current_date = theDate;
+
+        this.element.datepicker('setDate', this.current_date);
+
+        this.input_element.val(this._format_date(this.current_date));
+    },
+
+    _format_date: function(theDate) {
+        return $.datepicker.formatDate(this.options.dateFormat, theDate);
+    },
+    _parse_date: function(theDate) {
+        return $.datepicker.parseDate(this.options.dateFormat, theDate);
+    },
+
+    _get_first_day_of_month:function (year, month) {
+        return this._parse_date("1-" + month + "-" + year);
+    },
+    _get_last_day_of_month:function(year, month){
+        //calculate the last day of the month
+        //see http://javascript.about.com/library/bllday.htm
+        return new Date(year, month, 0);
+    }
 });
