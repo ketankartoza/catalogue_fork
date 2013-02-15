6fa362f47cf054c4a8e9f03bde1161369881957e
george <catalogue@catalogue-vm.(none)>
2013-02-15 13:20:03 +0200

    fixed datepicker and changed background slider to two minutes

--- django_project/sansa_catalogue/media/supersized/js/sansa-slideshow.js
+++ django_project/sansa_catalogue/media/supersized/js/sansa-slideshow.js
@@ -4,7 +4,7 @@ jQuery(function($){
         
           // Functionality
 
-          slide_interval          :   10000,   // Length between transitions
+          slide_interval          :   120000,   // Length between transitions
           transition              :   1,      // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
           transition_speed    : 700,    // Speed of transition
                                  
