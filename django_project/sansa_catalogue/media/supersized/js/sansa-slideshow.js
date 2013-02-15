efbe62a519011c5635c0501bfdee782f5c45df05
george <catalogue@catalogue-vm.(none)>
2013-02-15 14:31:17 +0200

    added Linda's bg images as samples and temporarily shortened bg change time

--- django_project/sansa_catalogue/media/supersized/js/sansa-slideshow.js
+++ django_project/sansa_catalogue/media/supersized/js/sansa-slideshow.js
@@ -4,7 +4,7 @@ jQuery(function($){
         
           // Functionality
 
-          slide_interval          :   120000,   // Length between transitions
+          slide_interval          :   3000,   // Length between transitions
           transition              :   1,      // 0-None, 1-Fade, 2-Slide Top, 3-Slide Right, 4-Slide Bottom, 5-Slide Left, 6-Carousel Right, 7-Carousel Left
           transition_speed    : 700,    // Speed of transition
                                  
@@ -19,8 +19,9 @@ jQuery(function($){
                                  
           // Components
           slides          :   [     // Slideshow Images
-                            {image : '/media/supersized/bgs/1.jpg'},
-                            {image : '/media/supersized/bgs/2.jpg'}
+                            {image : '/media/supersized/bgs/kflood.jpg'},
+                            {image : '/media/supersized/bgs/kruger.jpg'},
+                            {image : '/media/supersized/bgs/kflood2.jpg'}
                         ]
           
         });
