db6bf2b26207d81b662498b76fb1938fb83422f2
Tim Sutton <tim@linfiniti.com>
2013-02-20 14:02:38 +0200

    Improvements to create model graph script

--- scripts/create_model_graph.sh
+++ scripts/create_model_graph.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-python manage.py graph_models catalogue | dot -Tpng -o model_diagram.png ; display model_diagram.png
+cd django_project
+python manage.py graph_models catalogue --settings=sansa_catalogue.settings.dev_${USER} | dot -Tpng -o ../model_diagram.png ; display ../model_diagram.png
+python manage.py graph_models dictionaries --settings=sansa_catalogue.settings.dev_${USER} | dot -Tpng -o ../dict_model_diagram.png ; display ../dict_model_diagram.png
+cd ..
