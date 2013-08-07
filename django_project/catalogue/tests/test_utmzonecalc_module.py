7abd45ffd5ca8d2dcf7a226e415ec68a96dc8f5b
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2013-08-07 15:24:23 +0200

    Fixed UtmZonecalc module test
    Issue #65

--- django_project/catalogue/tests/test_utmzonecalc_module.py
+++ django_project/catalogue/tests/test_utmzonecalc_module.py
@@ -14,12 +14,12 @@ Contact : lkleyn@sansa.org.za
 """
 
 __author__ = 'dodobasic@gmail.com'
-__version__ = '0.1'
-__date__ = '10/07/2012'
+__version__ = '0.2'
+__date__ = '07/08/2013'
 __copyright__ = 'South African National Space Agency'
 
 from django.test import TestCase
-from catalogue.tests.test_utils import simpleMessage
+
 from catalogue.utmzonecalc import utmZoneFromLatLon
 
 
@@ -37,32 +37,39 @@ class utmZoneFromLatLon_Test(TestCase):
 
         for idx, testVal in enumerate(myTestValues):
             #check if exception is raised
-            self.assertRaises(ValueError, utmZoneFromLatLon, testVal[0], testVal[1])
+            self.assertRaises(
+                ValueError, utmZoneFromLatLon, testVal[0], testVal[1])
 
     def test_returnResult(self):
         """
         Tests if utmZoneFromLatLon will throw an exception on wrong input
         """
         myTestValues = [(-179, 0), (0, -80), (179, 89), (0, 0)]
-        myExpectedResults = [[('32701', 'UTM01S')], [('32731', 'UTM31S')],
-        [('32660', 'UTM60N')], [('32731', 'UTM31S')]]
+        myExpectedResults = [
+            [('32701', 'UTM01S')], [('32731', 'UTM31S')],
+            [('32660', 'UTM60N')], [('32731', 'UTM31S')]
+        ]
 
         for idx, testVal in enumerate(myTestValues):
             myRes = utmZoneFromLatLon(*testVal)
             myExpRes = myExpectedResults[idx]
 
-            self.assertEqual(myRes, myExpRes, simpleMessage(myRes, myExpRes))
+            self.assertEqual(myRes, myExpRes)
 
     def test_returnResultWithBuffer(self):
         """
         Tests if utmZoneFromLatLon will throw an exception on wrong input
         """
         myTestValues = [(-32, -64, 0), (-32, -64, 1), (-32, -64, 2)]
-        myExpectedResults = [[('32725', 'UTM25S')], [('32724', 'UTM24S'), ('32725', 'UTM25S'), ('32726', 'UTM26S')],
-                [('32723', 'UTM23S'), ('32724', 'UTM24S'), ('32725', 'UTM25S'), ('32726', 'UTM26S'), ('32727', 'UTM27S')]]
+        myExpectedResults = [
+            [('32725', 'UTM25S')],
+            [('32724', 'UTM24S'), ('32725', 'UTM25S'), ('32726', 'UTM26S')],
+            [('32723', 'UTM23S'), ('32724', 'UTM24S'), ('32725', 'UTM25S'),
+                ('32726', 'UTM26S'), ('32727', 'UTM27S')]
+        ]
 
         for idx, testVal in enumerate(myTestValues):
             myRes = utmZoneFromLatLon(*testVal)
             myExpRes = myExpectedResults[idx]
 
-            self.assertEqual(myRes, myExpRes, simpleMessage(myRes, myExpRes))
+            self.assertEqual(myRes, myExpRes)
