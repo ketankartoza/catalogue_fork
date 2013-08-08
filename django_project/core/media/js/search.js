68bb604464cb46c6fa5c748e702dc2b19aa6544a
Igor Crnković <17ender@gmail.com>
2013-08-08 15:47:13 +0200

    added cart panel, panel toggle mechanic Issue #64

---
+++ django_project/core/media/js/search.js
@@ -0,0 +1,58 @@
+var map = new OpenLayers.Map( 'map', {controls: []});
+var SearchPanelState = false;
+var CartPanelState = false;
+
+function initMap() {
+    var layer = new OpenLayers.Layer.OSM( "Simple OSM Map");
+    map.addLayer(layer);
+    map.setCenter(
+        new OpenLayers.LonLat(-71.147, 42.472).transform(
+            new OpenLayers.Projection("EPSG:4326"),
+            map.getProjectionObject()
+        ), 12
+    );
+}
+
+function toggleSearchPanel() {
+    if (SearchPanelState) {
+        $("#search-panel").animate({left: -300}, 300 );
+        SearchPanelState = false;
+        $("#cart-panel-toggle").animate({top: 130}, 200 );
+    } else {
+        if (CartPanelState) {
+            closeCartPanel();
+        }
+        $("#search-panel").animate({left: 10}, 300 );
+        SearchPanelState = true;
+        $("#cart-panel-toggle").animate({top: 515}, 200 );
+    }
+}
+
+function closeSearchPanel() {
+    $("#search-panel").animate({left: -300}, 300 );
+    SearchPanelState = false;
+    $("#cart-panel-toggle").animate({top: 130}, 200 );
+}
+
+function toggleCartPanel() {
+    if (CartPanelState) {
+        $("#cart-panel").animate({left: -300}, 300 );
+        CartPanelState = false;
+        $("#cart-panel-toggle").animate({top: 130}, 200 );
+        $("#search-panel-toggle").animate({top: 0}, 200 );
+    } else {
+        if (SearchPanelState) {
+            closeSearchPanel();
+        }
+        $("#cart-panel").animate({left: 10}, 300 );
+        CartPanelState = true;
+        $("#cart-panel-toggle").animate({top: 0}, 200 );
+        $("#search-panel-toggle").animate({top: 515}, 200 );
+    }
+}
+
+function closeCartPanel() {
+    $("#cart-panel").animate({left: -300}, 300 );
+    CartPanelState = false;
+    $("#search-panel-toggle").animate({top: 0}, 200 );
+}
