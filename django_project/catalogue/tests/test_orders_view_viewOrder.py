f856220c7046b942472d4682aae75606b4101418
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2013-08-14 15:13:09 +0200

    Fixed viewOrder order view test
    Issue #65

--- django_project/catalogue/tests/test_orders_view_viewOrder.py
+++ django_project/catalogue/tests/test_orders_view_viewOrder.py
@@ -14,70 +14,26 @@ Contact : lkleyn@sansa.org.za
 """
 
 __author__ = 'tim@linfiniti.com'
-__version__ = '0.1'
-__date__ = '23/10/2012'
+__version__ = '0.2'
+__date__ = '14/08/2013'
 __copyright__ = 'South African National Space Agency'
 
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django.test import TestCase
 from django.test.client import Client
 
-from catalogue.models import (
-    Order,
-    OrderStatusHistory,
-)
-
-from search.models import SearchRecord
 
 from catalogue.forms import OrderStatusHistoryForm
 
-from catalogue.views.orders import coverageForOrder
+from core.model_factories import UserF
+from search.tests.model_factories import SearchRecordF
+from .model_factories import OrderF, OrderStatusHistoryF
 
 
 class OrdersViews_viewOrder_Tests(TestCase):
     """
     Tests orders.py viewOrder method/view
     """
-
-    fixtures = [
-        'test_institution.json',
-        'test_license.json',
-        'test_projection.json',
-        'test_quality.json',
-        'test_creatingsoftware.json',
-        'test_search.json',
-        'test_searchdaterange.json',
-        'test_processinglevel.json',
-        # new_dicts
-        'test_radarbeam.json',
-        'test_imagingmode.json',
-        'test_spectralgroup.json',
-        'test_spectralmode.json',
-        'test_scannertype.json',
-        'test_instrumenttype.json',
-        'test_collection.json',
-        'test_satellite.json',
-        'test_satelliteinstrument.json',
-        'test_radarproductprofile.json',
-        'test_opticalproductprofile.json',
-
-        'test_genericproduct.json',
-        'test_genericimageryproduct.json',
-        'test_genericsensorproduct.json',
-        'test_opticalproduct.json',
-        'test_user.json',
-        'test_orderstatus.json',
-        'test_orderstatushistory.json',
-        'test_marketsector.json',
-        'test_deliverymethod.json',
-        'test_order.json',
-        'test_searchrecord.json',
-        'test_deliverymethod.json',
-        'test_deliverydetail.json',
-        'test_datum.json',
-        'test_resamplingmethod.json',
-        'test_fileformat.json',
-    ]
 
     def setUp(self):
         """
@@ -113,26 +69,26 @@ class OrdersViews_viewOrder_Tests(TestCase):
         """
         Test view if user is staff
         """
+        UserF.create(**{
+            'username': 'timlinux',
+            'password': 'password',
+            'is_staff': True
+        })
 
-        myOrderPk = 1
-        myOrderObj = Order.objects.get(pk=myOrderPk)
-        mySearchRecords = SearchRecord.objects.all().filter(order=myOrderObj)
-        myFormObj = OrderStatusHistoryForm()
-        myHistoryObj = OrderStatusHistory.objects.all().filter(
-            order=myOrderObj)
-        myCoverageObj = coverageForOrder(myOrderObj, mySearchRecords)
+        myOrder = OrderF.create(**{'id': 1})
+        OrderStatusHistoryF.create(**{'order': myOrder})
+
+        SearchRecordF.create(**{'order': myOrder})
 
         myClient = Client()
         myClient.login(username='timlinux', password='password')
-        myResp = myClient.get(
-            reverse('viewOrder', kwargs={'theId': myOrderPk}))
+        myResp = myClient.get(reverse('viewOrder', kwargs={'theId': 1}))
 
         self.assertEqual(myResp.status_code, 200)
 
         # check response object
-        self.assertEqual(myResp.context['myOrder'], myOrderObj)
-        self.assertEqual(
-            len(myResp.context['myRecords']), len(mySearchRecords))
+        self.assertEqual(myResp.context['myOrder'], myOrder)
+        self.assertEqual(len(myResp.context['myRecords']), 1)
         self.assertEqual(myResp.context['myShowSensorFlag'], False)
         self.assertEqual(myResp.context['myShowSceneIdFlag'], True)
         self.assertEqual(myResp.context['myShowDateFlag'], False)
@@ -146,19 +102,25 @@ class OrdersViews_viewOrder_Tests(TestCase):
             myResp.context['myShowDeliveryDetailsFormFlag'], False)
         self.assertEqual(myResp.context['myDownloadOrderFlag'], True)
         self.assertEqual(
-            myResp.context['myForm'].__class__, myFormObj.__class__)
-        self.assertEqual(len(myResp.context['myHistory']), len(myHistoryObj))
+            myResp.context['myForm'].__class__, OrderStatusHistoryForm)
+
+        self.assertEqual(len(myResp.context['myHistory']), 1)
         self.assertEqual(myResp.context['myCartTitle'], 'Product List')
-        self.assertEqual(myResp.context['myCoverage'], myCoverageObj)
+        self.assertEqual(myResp.context['myCoverage'], {
+            'IntersectedArea': 'Not applicable', 'ClipZone': 'Not applicable',
+            'CentroidZone': 'UTM34S (EPSG:32734)',
+            'ProductArea': 79894321621.06012
+        })
 
         # check used templates
         myExpTemplates = [
             'orderPage.html', u'base.html', u'menu.html',
             u'useraccounts/menu_content.html', u'order.html',
             u'cartContents.html', u'recordHeader.html', u'record.html',
-            u'record.html', u'record.html', u'orderStatusHistory.html']
+            u'orderStatusHistory.html'
+        ]
 
-        myUsedTemplates = [tmpl.name for tmpl in myResp.template]
+        myUsedTemplates = [tmpl.name for tmpl in myResp.templates]
         self.assertEqual(myUsedTemplates, myExpTemplates)
 
     def test_viewOrder_login_user(self):
@@ -166,25 +128,26 @@ class OrdersViews_viewOrder_Tests(TestCase):
         Test view if user is normal user
         """
 
-        myOrderPk = 2
-        myOrderObj = Order.objects.get(pk=myOrderPk)
-        mySearchRecords = SearchRecord.objects.all().filter(order=myOrderObj)
-        myFormObj = None
-        myHistoryObj = OrderStatusHistory.objects.all().filter(
-            order=myOrderObj)
-        myCoverageObj = coverageForOrder(myOrderObj, mySearchRecords)
+        myUser = UserF.create(**{
+            'username': 'pompies',
+            'password': 'password'
+        })
+
+        myOrder = OrderF.create(**{'id': 1, 'user': myUser})
+        OrderStatusHistoryF.create(**{'order': myOrder})
+
+        SearchRecordF.create(**{'order': myOrder})
 
         myClient = Client()
         myClient.login(username='pompies', password='password')
         myResp = myClient.get(
-            reverse('viewOrder', kwargs={'theId': myOrderPk}))
+            reverse('viewOrder', kwargs={'theId': 1}))
 
         self.assertEqual(myResp.status_code, 200)
 
         # check response object
-        self.assertEqual(myResp.context['myOrder'], myOrderObj)
-        self.assertEqual(
-            len(myResp.context['myRecords']), len(mySearchRecords))
+        self.assertEqual(myResp.context['myOrder'], myOrder)
+        self.assertEqual(len(myResp.context['myRecords']), 1)
         self.assertEqual(myResp.context['myShowSensorFlag'], False)
         self.assertEqual(myResp.context['myShowSceneIdFlag'], True)
         self.assertEqual(myResp.context['myShowDateFlag'], False)
@@ -197,32 +160,40 @@ class OrdersViews_viewOrder_Tests(TestCase):
         self.assertEqual(
             myResp.context['myShowDeliveryDetailsFormFlag'], False)
         self.assertEqual(myResp.context['myDownloadOrderFlag'], True)
-        self.assertEqual(
-            myResp.context['myForm'].__class__, myFormObj.__class__)
-        self.assertEqual(len(myResp.context['myHistory']), len(myHistoryObj))
+        self.assertEqual(myResp.context['myForm'], None)
+        self.assertEqual(len(myResp.context['myHistory']), 1)
         self.assertEqual(myResp.context['myCartTitle'], 'Product List')
-        self.assertEqual(myResp.context['myCoverage'], myCoverageObj)
+        self.assertEqual(myResp.context['myCoverage'], {
+            'IntersectedArea': 'Not applicable', 'ClipZone': 'Not applicable',
+            'CentroidZone': 'UTM34S (EPSG:32734)',
+            'ProductArea': 79894321621.06012
+        })
 
         # check used templates
         myExpTemplates = [
             'orderPage.html', u'base.html', u'menu.html',
             u'useraccounts/menu_content.html', u'order.html',
-            u'cartContents.html']
+            u'cartContents.html', u'recordHeader.html', u'record.html',
+            u'orderStatusHistory.html'
+        ]
 
-        myUsedTemplates = [tmpl.name for tmpl in myResp.template]
+        myUsedTemplates = [tmpl.name for tmpl in myResp.templates]
         self.assertEqual(myUsedTemplates, myExpTemplates)
 
     def test_viewOrder_login_user_notowned(self):
         """
         Test view if user is normal user, not owned order
         """
-
-        myOrderPk = 1
+        UserF.create(**{
+            'username': 'pompies',
+            'password': 'password'
+        })
+        OrderF.create(**{'id': 1})
 
         myClient = Client()
         myClient.login(username='pompies', password='password')
         myResp = myClient.get(
-            reverse('viewOrder', kwargs={'theId': myOrderPk}))
+            reverse('viewOrder', kwargs={'theId': 1}))
 
         self.assertEqual(myResp.status_code, 404)
 
@@ -230,26 +201,28 @@ class OrdersViews_viewOrder_Tests(TestCase):
         """
         Test view if user is staff, and request is ajax
         """
-        myOrderPk = 2
-        myOrderObj = Order.objects.get(pk=myOrderPk)
-        mySearchRecords = SearchRecord.objects.all().filter(order=myOrderObj)
-        myFormObj = OrderStatusHistoryForm()
-        myHistoryObj = OrderStatusHistory.objects.all().filter(
-            order=myOrderObj)
-        myCoverageObj = coverageForOrder(myOrderObj, mySearchRecords)
+        UserF.create(**{
+            'username': 'timlinux',
+            'password': 'password',
+            'is_staff': True
+        })
+
+        myOrder = OrderF.create(**{'id': 1})
+        OrderStatusHistoryF.create(**{'order': myOrder})
+
+        SearchRecordF.create(**{'order': myOrder})
 
         myClient = Client()
         myClient.login(username='timlinux', password='password')
         myResp = myClient.get(
-            reverse('viewOrder', kwargs={'theId': myOrderPk}),
+            reverse('viewOrder', kwargs={'theId': 1}),
             HTTP_X_REQUESTED_WITH='XMLHttpRequest')
 
         self.assertEqual(myResp.status_code, 200)
 
         # check response object
-        self.assertEqual(myResp.context['myOrder'], myOrderObj)
-        self.assertEqual(
-            len(myResp.context['myRecords']), len(mySearchRecords))
+        self.assertEqual(myResp.context['myOrder'], myOrder)
+        self.assertEqual(len(myResp.context['myRecords']), 1)
         self.assertEqual(myResp.context['myShowSensorFlag'], False)
         self.assertEqual(myResp.context['myShowSceneIdFlag'], True)
         self.assertEqual(myResp.context['myShowDateFlag'], False)
@@ -263,15 +236,20 @@ class OrdersViews_viewOrder_Tests(TestCase):
             myResp.context['myShowDeliveryDetailsFormFlag'], False)
         self.assertEqual(myResp.context['myDownloadOrderFlag'], True)
         self.assertEqual(
-            myResp.context['myForm'].__class__, myFormObj.__class__)
-        self.assertEqual(len(myResp.context['myHistory']), len(myHistoryObj))
+            myResp.context['myForm'].__class__, OrderStatusHistoryForm)
+        self.assertEqual(len(myResp.context['myHistory']), 1)
         self.assertEqual(myResp.context['myCartTitle'], 'Product List')
-        self.assertEqual(myResp.context['myCoverage'], myCoverageObj)
-
+        self.assertEqual(myResp.context['myCoverage'], {
+            'IntersectedArea': 'Not applicable', 'ClipZone': 'Not applicable',
+            'CentroidZone': 'UTM34S (EPSG:32734)',
+            'ProductArea': 79894321621.06012
+        })
         # check used templates
         myExpTemplates = [
             'orderPageAjax.html', u'emptytemplate.html', u'order.html',
-            u'cartContents.html']
+            u'cartContents.html', u'recordHeader.html', u'record.html',
+            u'orderStatusHistory.html'
+        ]
 
-        myUsedTemplates = [tmpl.name for tmpl in myResp.template]
+        myUsedTemplates = [tmpl.name for tmpl in myResp.templates]
         self.assertEqual(myUsedTemplates, myExpTemplates)
