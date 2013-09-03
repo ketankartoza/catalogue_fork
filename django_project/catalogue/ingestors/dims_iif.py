02e7c18bb0352af57268920f28df41ed5054412b
Tim Sutton <tim@linfiniti.com>
2013-09-03 14:26:27 +0200

    #85 We need a new generic dims ingestor - fix code formatting

--- django_project/catalogue/ingestors/dims_iif.py
+++ django_project/catalogue/ingestors/dims_iif.py
@@ -20,7 +20,7 @@ __copyright__ = 'South African National Space Agency'
 import os
 import sys
 import glob
-from datetime import datetime, timedelta
+from datetime import datetime  # , timedelta
 from xml.dom.minidom import parse
 import traceback
 
@@ -46,14 +46,13 @@ from catalogue.models import (
 def parseDateTime(theDate):
     """A helper method to create a date object from a landsat time stamp.
 
-    Args:
-        theDate: str - a string containing date in this format:
-            :samp:`19890503 07:30:05`
-	    :IIF samp:`1989-05-03T07:30:05.000`
-    Returns:
-        datetime - a python datetime object
-    Raises:
-        None
+    :param theDate: Date in this format:
+    :type theDate: str
+
+    Example format from IIF:`1989-05-03T07:30:05.000`
+
+    :returns: A python datetime object.
+    :rtype: datetime
     """
     #print 'Parsing Date: %s\n' % theDate
     myStartYear = theDate[0:4]
@@ -80,7 +79,9 @@ def parseDateTime(theDate):
 @transaction.commit_manually
 def ingest(
         theTestOnlyFlag=True,
-        theSourceDir= '/home/web/catalogue/django_project/catalogue/tests/sample_files/landsat/',
+        theSourceDir=(
+            '/home/web/catalogue/django_project/catalogue/tests/sample_files/'
+            'landsat/'),
         theVerbosityLevel=2,
         theHaltOnErrorFlag=True):
     """
@@ -90,10 +91,10 @@ def ingest(
         * theTestOnlyFlag - (Optional) Defaults to False. Whether to do a dummy
            run (database will not be updated).
         * theSourceDir - (Required) A DIMS created IIF metadata xml file and
-	   thumbnail
+          thumbnail
         * theVerbosityLevel - (Optional) Defaults to 1. How verbose the logging
            output should be. 0-2 where 2 is very very very very verbose!
-        * theLicense - (Optional) Defaults to 'SANSA Free License',
+        * myLicense - (Optional) Defaults to 'SANSA Free License',
             License holder of the product.
         * theOwner - (Optional) Defaults to 'USGS', Original provider / owner
            of the data.
@@ -101,7 +102,7 @@ def ingest(
             create / extract the product.
         * theSoftwareVersion - str (Optional) Defaults to 11.6.0.
         * theQuality - (Optional) Defaults to 'Unknown', A quality assessment
-            for these images defined in the IIF file. 
+            for these images defined in the IIF file.
         * theHaltOnErrorFlag: bool - set to True if we should stop processing
             when the first error is encountered.
     Returns:
@@ -120,7 +121,7 @@ def ingest(
         'Verbosity Level: %s\n'
         'Halt on error: %s\n'
         '------------------')
-        % (theTestOnlyFlag, theSourceDir, theVerbosityLevel, 
+        % (theTestOnlyFlag, theSourceDir, theVerbosityLevel,
            theHaltOnErrorFlag), 2)
 
     # Scan the source folder and look for any sub-folders
@@ -136,12 +137,12 @@ def ingest(
 
     for myFolder in glob.glob(os.path.join(theSourceDir, '*')):
         myRecordCount += 1
-        
-	# Get the folder name
+
+        # Get the folder name
         myProductFolder = os.path.split(myFolder)[-1]
         logMessage(myProductFolder, 2)
-       
-	 # Find the first and only xml file in the folder
+
+        # Find the first and only xml file in the folder
         mySearchPath = os.path.join(str(myFolder), '*.xml')
         logMessage(mySearchPath, 2)
         myXmlFile = glob.glob(mySearchPath)[0]
@@ -149,93 +150,98 @@ def ingest(
 
         # Create a DOM document from the file
         myDom = parse(myXmlFile)
-	
-	# Get the owner - maybe fetch this via the OpticalProductProfile
-        # if theOwner is None? TS  
+
+        # Get the owner - maybe fetch this via the OpticalProductProfile
+        # if theOwner is None? TS
         myElement = myDom.getElementsByTagName('owner')[0]
         theOwner = myElement.firstChild.nodeValue
-        logMessage('Owner: %s' % theOwner, 2) 
-	try:
-	    myOwner = Institution.objects.get_or_create(
-		name=theOwner,
-		defaults={'address1': '',
-		          'address2': '',
-		          'address3': '',
-		          'post_code': '', })[0]
-	except Institution.DoesNotExist:
-	    #logMessage('Institution %s does not exists and '
-	    #         'cannot be created.' % owner, 2)
-	raise CommandError(
-	    'Institution %s does not exist and '
-	    'cannot create: aborting' % theOwner)
-	logMessage('Owner: %s' % myOwner)
+        logMessage('Owner: %s' % theOwner, 2)
+        try:
+            myOwner = Institution.objects.get_or_create(
+                name=theOwner,
+                defaults={
+                    'address1': '',
+                    'address2': '',
+                    'address3': '',
+                    'post_code': '', })[0]
+        except Institution.DoesNotExist:
+            #logMessage('Institution %s does not exists and '
+            #         'cannot be created.' % owner, 2)
+            raise CommandError(
+                'Institution %s does not exist and '
+                'cannot create: aborting' % theOwner)
+        logMessage('Owner: %s' % myOwner)
 
-	# Get the license - maybe fetch this via the OpticalProductProfile
-	# if license is None? TS
-	myElement = myDom.getElementsByTagName('license')[0]
-        theLicense = myElement.firstChild.nodeValue
-        logMessage('License: %s' % theLicense, 2) 
-	myLicenseMapping = {
+        # Get the license - maybe fetch this via the OpticalProductProfile
+        # if license is None? TS
+        myElement = myDom.getElementsByTagName('license')[0]
+        myLicense = myElement.firstChild.nodeValue
+        logMessage('License: %s' % myLicense, 2)
+        myLicenseMapping = {
             'USGS free distribution license': 'SANSA Free License'
-   	         	    }
+        }
         theSANSALicense = License.objects.get(
-              name=myLicenseMapping[myLicenseName])
-        logMessage('License: %s' % theLicense, 2)
-	logMessage('SANSA License: %s' % theSANSALicense, 2)
-	try:
-	    myLicense = License.objects.get_or_create(
-		name=theSANSALicense,
-	        details=theLicense)[0]
-	except License.DoesNotExist:
-	raise CommandError(
-	    'License %s does not exist and '
-	    'cannot create: aborting' % theOwner)
-	logMessage('License: %s' % myLicense)
+            name=myLicenseMapping[myLicense])
+        logMessage('License: %s' % myLicense, 2)
+        logMessage('SANSA License: %s' % theSANSALicense, 2)
 
-        # Get the creating software - maybe fetch this via the 
+        try:
+            myLicense = License.objects.get_or_create(
+                name=theSANSALicense,
+                details=myLicense)[0]
+        except License.DoesNotExist:
+            raise CommandError(
+                'License %s does not exist and '
+                'cannot create: aborting' % theOwner)
+        logMessage('License: %s' % myLicense)
+
+        # Get the creating software - maybe fetch this via the
         # OpticalProductProfile if theOwner is None? TS
         myElement = myDom.getElementsByTagName('softwareInfo.name')[0]
-        theSoftware = myElement.firstChild.nodeValue
-        logMessage('Software: %s' % theSoftware, 2) 
-	mySoftwareMapping = {
-            'Pinkmatter Landsat Processor': 'LPGS'
-   	         	    }
-        theSoftware = CreatingSoftware.objects.get(
-              name=mySoftwareMapping[mySoftwareName])
+        mySoftware = myElement.firstChild.nodeValue
         logMessage('Software: %s' % mySoftware, 2)
-	myElement = myDom.getElementsByTagName('softwareInfo.version')[0]
+        mySoftwareMapping = {
+            'Pinkmatter Landsat Processor': 'LPGS'
+        }
+        theSoftware = CreatingSoftware.objects.get(
+            name=mySoftwareMapping[mySoftware])
+
+        logMessage('Software: %s' % mySoftware, 2)
+        myElement = myDom.getElementsByTagName('softwareInfo.version')[0]
         theSoftwareVersion = myElement.firstChild.nodeValue
-        logMessage('Software Version: %s' % theSoftwareVersion, 2) 
-	try:
+        logMessage('Software Version: %s' % theSoftwareVersion, 2)
+
+        try:
             mySoftware = CreatingSoftware.objects.get_or_create(
                 name=theSoftware,
                 version=theSoftwareVersion)[0]
         except CreatingSoftware.DoesNotExist:
-        raise CommandError(
-            'Software %s does not exist and '
-            'cannot create: aborting' % theOwner)
+            raise CommandError(
+                'Software %s does not exist and '
+                'cannot create: aborting' % theOwner)
+
         logMessage('Software: %s' % mySoftware)
 
-	# Get the quality assessment. 
-	myElement = myDom.getElementsByTagName('quality')[0]
+        # Get the quality assessment.
+        myElement = myDom.getElementsByTagName('quality')[0]
         theQuality = myElement.firstChild.nodeValue
-        logMessage('Quality: %s' % theQuality, 2) 
-	try:
-	    myQuality = Quality.objects.get_or_create(name=theQuality)[0]
-	except Quality.DoesNotExist:
-	    #logMessage(
-	    #	'Quality %s does not exists and cannot be created,'
-	    #	' it will be read from metadata.' % theQuality, 2)
-	raise CommandError(
-		'Quality %s does not exists and cannot '
-		' be created: aborting' % theQuality)
-	logMessage('Quality: %s' % myQuality)
+        logMessage('Quality: %s' % theQuality, 2)
 
+        try:
+            myQuality = Quality.objects.get_or_create(name=theQuality)[0]
+        except Quality.DoesNotExist:
+            #logMessage(
+            #'Quality %s does not exists and cannot be created,'
+            #' it will be read from metadata.' % theQuality, 2)
+            raise CommandError(
+                'Quality %s does not exists and cannot '
+                ' be created: aborting' % theQuality)
+        logMessage('Quality: %s' % myQuality)
 
-	# Look for key concepts needed to create an OpticalProduct
+        # Look for key concepts needed to create an OpticalProduct
         myElement = myDom.getElementsByTagName('temporalCoverage.centerTime')[0]
         myProductDate = myElement.firstChild.nodeValue
-	myMidDateTime = myElement.firstChild.nodeValue
+        myMidDateTime = myElement.firstChild.nodeValue
         logMessage('Product Date: %s' % myProductDate, 2)
 
         myElement = myDom.getElementsByTagName('temporalCoverage.startTime')[0]
@@ -243,22 +249,22 @@ def ingest(
         myStartDateTime = parseDateTime(myStartDate)
         logMessage('Product Start Date: %s' % myStartDateTime, 2)
 
-	logMessage('Mid Scene Date: %s' % myMidDateTime, 2)
+        logMessage('Mid Scene Date: %s' % myMidDateTime, 2)
 
         myElement = myDom.getElementsByTagName('temporalCoverage.stopTime')[0]
         myEndDate = myElement.firstChild.nodeValue
         myEndDateTime = parseDateTime(myEndDate)
         logMessage('Product End Date: %s' % myEndDateTime, 2)
-        
-	# Get the spectral mode 
+
+        # Get the spectral mode
         myElement = myDom.getElementsByTagName('sensor')[0]
-        mySpectralMode = myElement.firstChild.nodeValue
-        logMessage('Spectral Mode: %s' % mySpectralMode, 2)
-        # Need to get the Spectral mode used for 
+        mySpectralModeName = myElement.firstChild.nodeValue
+        logMessage('Spectral Mode: %s' % mySpectralModeName, 2)
+        # Need to get the Spectral mode used for
         # Landsat 8, spectral mode is OLI_TIRS HRF
         # Landsat 8, spectral mode is OLI HRF
         # Landsat 8, spectral mode is TIRS THM
-	# Landsat 7, spectral mode is ETM+ HRF
+        # Landsat 7, spectral mode is ETM+ HRF
         # Landsat 5, spectral mode is TM HRF
         # Landsat 5, spectral mode is MSS HRF
         #
@@ -268,24 +274,24 @@ def ingest(
         # 4 | TM HRF   | Landsat 4 or 5 TM Multi-spectral bands denoted as HRF
         mySpectralModeMapping = {
             'MSS': 'MSS HRF',
-	    'TM': 'TM HRF',
+            'TM': 'TM HRF',
             'ETM+': 'ETM+ HRF',
-	    'OLI_TIRS': 'OLI_TIRS HRF',
-	    'OLI': 'OLI HRF',
-	    'TiRS': 'TIRS THM',}
+            'OLI_TIRS': 'OLI_TIRS HRF',
+            'OLI': 'OLI HRF',
+            'TiRS': 'TIRS THM'}
         mySpectralMode = SpectralMode.objects.get(
             name=mySpectralModeMapping[mySpectralModeName])
         logMessage('Spectral Mode: %s' % mySpectralMode, 2)
-        
-	# Get the instrument name from the metadata so we can
+
+        # Get the instrument name from the metadata so we can
         # determine if this is L8, L7 or L5
         myElement = myDom.getElementsByTagName('mission')[0]
         myInstrumentName = myElement.firstChild.nodeValue
         logMessage('Instrument Name: %s' % myInstrumentName, 2)
 
-# *** Continue from here
+        # *** Continue from here
 
-	# And the SatelliteInstrument
+        # And the SatelliteInstrument
         #id |          name
         #----+------------------------
         # ...
@@ -305,17 +311,19 @@ def ingest(
             spectral_mode=mySpectralMode,
             satellite_instrument=mySatelliteInstrument)
         logMessage('Spectral Profile: %s' % myProfile, 2)
-        
-	# Get the base processing level
+
+        # Get the base processing level
         myProcessingLevel = myProfile.baseProcessingLevel()
         logMessage('Processing Level: %s' % myProcessingLevel, 2)
-        
-	# Get the spatial coverage
-        myElement = myDom.getElementsByTagName('spatialCoverage.boundingPolygon.latitiude')[0]
+
+        # Get the spatial coverage
+        myElement = myDom.getElementsByTagName(
+            'spatialCoverage.boundingPolygon.latitiude')[0]
         myUpperLeftLat = myElement.firstChild.nodeValue
         logMessage('Upper Left Lat: %s' % myUpperLeftLat, 2)
 
-        myElement = myDom.getElementsByTagName('spatialCoverage.boundingPolygon.longitude')[0]
+        myElement = myDom.getElementsByTagName(
+            'spatialCoverage.boundingPolygon.longitude')[0]
         myUpperLeftLon = myElement.firstChild.nodeValue
         logMessage('Upper Left Lon: %s' % myUpperLeftLon, 2)
 
@@ -469,7 +477,6 @@ def ingest(
                 logMessage('Testing: image not saved.', 2)
                 pass
             else:
-
                 # Store thumbnail
                 myThumbsFolder = os.path.join(
                     settings.THUMBS_ROOT,
@@ -481,21 +488,19 @@ def ingest(
                     # attempt to  recreate an existing dir
                     pass
 
-                # Transform and store .wld file
-                logMessage('Referencing thumb', 2)
-                try:
-                    myPath = myProduct.georeferencedThumbnail()
-                    logMessage('Georeferenced Thumb: %s' % myPath, 2)
-                except:
-                    traceback.print_exc(file=sys.stdout)
+                    # Transform and store .wld file
+                    logMessage('Referencing thumb', 2)
+                    try:
+                        myPath = myProduct.georeferencedThumbnail()
+                        logMessage('Georeferenced Thumb: %s' % myPath, 2)
+                    except:
+                        traceback.print_exc(file=sys.stdout)
 
             if myNewRecordFlag:
-                logMessage('Product %s imported.' %
-                           myRecordCount, 2)
+                logMessage('Product %s imported.' % myRecordCount, 2)
                 pass
             else:
-                logMessage('Product %s updated.' %
-                           myUpdatedRecordCount, 2)
+                logMessage('Product %s updated.' % myUpdatedRecordCount, 2)
                 pass
         except Exception, e:
             traceback.print_exc(file=sys.stdout)
