05d34e39fe7e7bdf57d87df7855cff0b2a321a3e
Dražen Odobašić <dodobas@geoinfo.geof.hr>
2013-10-24 11:52:48 +0200

    Fixing Mr. Jenkins, second try
    Issue #115

--- django_project/core/settings/jenkins.py
+++ django_project/core/settings/jenkins.py
@@ -63,4 +63,6 @@ JENKINS_TASKS = (
 # commit 2f241bb6b7a111172f1e5bd26a1d21815f83d1e7
 JENKINS_TEST_RUNNER = 'django_jenkins.nose_runner.CINoseTestSuiteRunner'
 
-PIPELINE = DEBUG
+PIPELINE_YUGLIFY_BINARY = ABS_PATH(
+    '..', 'node_modules', 'yuglify', 'bin', 'yuglify'
+)
