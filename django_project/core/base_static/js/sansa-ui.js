0af1a563f6725d3bad63fce5d76decc76b34f026
Igor Crnković <17ender@gmail.com>
2013-10-24 15:35:40 +0200

    fixed data summary links
    Issue #36

--- django_project/core/base_static/js/sansa-ui.js
+++ django_project/core/base_static/js/sansa-ui.js
@@ -3,3 +3,31 @@ function zebraTables()
   $("table tr:even").addClass("even");
   $("table tr:odd").addClass("odd");
 }
+
+function loadContent( theUrl )
+{
+  uiBlock();
+  $("#main-content").load( theUrl ,"",uiUnblock);
+}
+
+function loadSummaryTable(theId)
+{
+  uiBlock();
+  $("#main-content").load("/sensorSummaryTable/" + theId + "/","",uiUnblock);
+}
+
+function uiBlock() {
+    $.blockUI({
+        message: '<div class="wrapperloading"><div class="loading up"></div><div class="loading down"></div></div>',
+        css: {
+            border: '1px solid #000',
+            background: 'rgba(0, 0, 0, 0.3)',
+            width: '550px',
+            height:'550px'
+        }
+    });
+}
+
+function uiUnblock() {
+    $.unblockUI();
+}
