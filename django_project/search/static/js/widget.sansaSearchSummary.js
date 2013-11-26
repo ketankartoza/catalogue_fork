5ae156ab45118c17bb120da3506cf97460419838
Igor Crnković <17ender@gmail.com>
2013-11-26 15:52:02 +0100

    all fields included in summary
    Issue #156

--- django_project/search/static/js/widget.sansaSearchSummary.js
+++ django_project/search/static/js/widget.sansaSearchSummary.js
@@ -13,7 +13,7 @@
             { 'id': 'ssd5', 'label': 'Other options', 'data': {}}
         ];
         this.template = [
-            '<table>',
+            '<table class="table table-striped">',
             // iterate each  row
             '<% _.each(context, function(row) { %>',
                 // if there is any data it means there is something to show
@@ -35,10 +35,19 @@
     APP.SearchSummary.prototype = {
 
         _initialize: function() {
-            // listening on listTree plugin to fire change event
+            // listening on events to fire updates
             $(document).on("listTreeChange", $.proxy(this._handleListTree, this));
-            // listening on sansaDateRange plugin to fire change event
             $(document).on("sansaDateRangeChanged", $.proxy(this._handleDateRange, this));
+            $('#id_aoi_geometry').on("blur", $.proxy(this._handleAoiGeometry, this));
+            $('#id_k_orbit_path').on("blur", $.proxy(this._handlePath, this));
+            $('#id_j_frame_row').on("blur", $.proxy(this._handleRow, this));
+            $('#id_cloud_mean').on("blur", $.proxy(this._handleCloud, this));
+            $('#id_sensor_inclination_angle_start').on("blur", $.proxy(this._handleAngleStart, this));
+            $('#id_sensor_inclination_angle_end').on("blur", $.proxy(this._handleAngleEnd, this));
+            $('#id_spatial_resolution').on("change", $.proxy(this._handleResolution, this));
+            $('#id_band_count').on("change", $.proxy(this._handleBand, this));
+            $('#id_free_imagery').on("change", $.proxy(this._handleImagery, this));
+            $('#id_panchromatic_imagery').on("change", $.proxy(this._handlePImagery, this));
         },
 
         _handleListTree: function() {
@@ -83,6 +92,176 @@
 
             // save data
             if (text != '') this.data[1].data.dateRange = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleAoiGeometry: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = '<b>Bbox: </b>' + e.target.value;
+            }
+
+            // remove existing
+            if (typeof this.data[2].data.bbox != undefined) delete this.data[2].data.bbox;
+
+            // save data
+            if (text != '') this.data[2].data.bbox = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handlePath: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = '<b>Path: </b>' + e.target.value;
+            }
+
+            // remove existing
+            if (typeof this.data[2].data.path != undefined) delete this.data[2].data.path;
+
+            // save data
+            if (text != '') this.data[2].data.path = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleRow: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = '<b>Row: </b>' + e.target.value;
+            }
+
+            // remove existing
+            if (typeof this.data[2].data.row != undefined) delete this.data[2].data.row;
+
+            // save data
+            if (text != '') this.data[2].data.row = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleCloud: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = e.target.value + '%';
+            }
+
+            // remove existing
+            if (typeof this.data[3].data.cloud != undefined) delete this.data[3].data.cloud;
+
+            // save data
+            if (text != '') this.data[3].data.cloud = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleAngleStart: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = '<b>Inclination start: </b>' + e.target.value;
+            }
+
+            // remove existing
+            if (typeof this.data[4].data.anglestart != undefined) delete this.data[4].data.anglestart;
+
+            // save data
+            if (text != '') this.data[4].data.anglestart = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleAngleEnd: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = '<b>Inclination end: </b>' + e.target.value;
+            }
+
+            // remove existing
+            if (typeof this.data[4].data.angleend != undefined) delete this.data[4].data.angleend;
+
+            // save data
+            if (text != '') this.data[4].data.angleend = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleResolution: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = '<b>Spatial resolution: </b>' + $(e.target).find('option:selected').text();
+            }
+
+            // remove existing
+            if (typeof this.data[4].data.resolution != undefined) delete this.data[4].data.resolution;
+
+            // save data
+            if (text != '') this.data[4].data.resolution = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleBand: function(e) {
+            var text = '';
+            // if its not empty create text
+            if (e.target.value != '') {
+                text = '<b>Band count: </b>' + $(e.target).find('option:selected').text();
+            }
+
+            // remove existing
+            if (typeof this.data[4].data.band != undefined) delete this.data[4].data.band;
+
+            // save data
+            if (text != '') this.data[4].data.band = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handleImagery: function(e) {
+            var text = '';
+            // if its not empty create text
+            if ($(e.target).attr('checked')) {
+                text = 'Free images only';
+            }
+
+            // remove existing
+            if (typeof this.data[4].data.imagery != undefined) delete this.data[4].data.imagery;
+
+            // save data
+            if (text != '') this.data[4].data.imagery = text;
+
+            // request render of specific element
+            this._render();
+        },
+
+        _handlePImagery: function(e) {
+            var text = '';
+            // if its not empty create text
+            if ($(e.target).attr('checked')) {
+                text = 'Panchromatic images only';
+            }
+
+            // remove existing
+            if (typeof this.data[4].data.pimagery != undefined) delete this.data[4].data.pimagery;
+
+            // save data
+            if (text != '') this.data[4].data.pimagery = text;
 
             // request render of specific element
             this._render();
