d5eaa435461f17eb09fd8f60c1212be8e25f5529
Igor Crnković <17ender@gmail.com>
2013-12-17 08:59:26 +0100

    Disable the search button until a date range has been added
    Issue #196

--- django_project/search/static/js/search.js
+++ django_project/search/static/js/search.js
@@ -278,10 +278,22 @@ function resetSearchForm() {
 
 // backbone models/collections/views
 
+
+
 APP.$modal = $('#ajax-modal');
 APP.$imagemodal = null;
 APP.guid = '';
 APP.selectedFeatureID = '';
+
+APP.checkDateRange = function(e) {
+    if (JSON.parse(e.dates).length > 0) {
+        $('#search_button').removeClass('hide');
+    } else {
+        $('#search_button').addClass('hide');
+    }
+};
+
+$(document).on("sansaDateRangeChanged", APP.checkDateRange);
 
 APP.ResultItem = Backbone.Model.extend({
     //urlRoot: '/api/v1/searchresults/6cfa079f-8be1-4029-a1eb-f80875a4e27c/'
