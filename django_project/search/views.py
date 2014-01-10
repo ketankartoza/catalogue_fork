7d6215e42345e5f643d2546765cc70ca5409a29c
Igor Crnković <17ender@gmail.com>
2014-01-10 18:19:22 +0100

    removed unused temlates
    Issue #201

--- django_project/search/views.py
+++ django_project/search/views.py
@@ -35,7 +35,6 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.http import (
     HttpResponse,
     Http404)
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 #from django.contrib.admin.views.decorators import staff_member_required
 from django.template import RequestContext
