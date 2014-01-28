fafa39193183ab023598fabd4b87e96796a6e04e
Igor Crnković <17ender@gmail.com>
2014-01-28 21:04:00 +0100

    moved geometry bounds control back to map panel
    Issue #208

--- django_project/search/static/js/widget.sansaGeoSearchLayer.js
+++ django_project/search/static/js/widget.sansaGeoSearchLayer.js
@@ -40,14 +40,14 @@
     this.myDrawingControl = new OpenLayers.Control.DrawFeature(this.geoSearch, OpenLayers.Handler.Polygon, {
       'displayClass': 'btn btn-info btn-large right icon-check-empty olControlDrawFeaturePolygon',
       'title': 'Capture polygon: left click to add points, double click to finish capturing',
-      div : OpenLayers.Util.getElement('search_geometry_controls'),
+      div : OpenLayers.Util.getElement('map-navigation'),
       eventListeners: modifyEventListeners
     });
 
     this.myModifyFeatureControl = new OpenLayers.Control.ModifyFeature(this.geoSearch, {
       'displayClass': 'btn btn-info btn-large right icon-edit olControlModifyFeature',
       'title': 'Modify polygon: left click to move/add points, hover and press <i>delete</i> to delete points',
-      div : OpenLayers.Util.getElement('search_geometry_controls'),
+      div : OpenLayers.Util.getElement('map-navigation'),
       eventListeners: modifyEventListeners
     });
 
@@ -62,14 +62,12 @@
       'displayClass': 'btn btn-info btn-large right icon-remove  destroyFeature',
       'title':'Delete polygon: deletes polygon',
       'layer': this.geoSearch,
-      div : OpenLayers.Util.getElement('search_geometry_controls'),
+      div : OpenLayers.Util.getElement('map-navigation'),
       eventListeners: modifyEventListeners
     });
-    this.mGeoControlPanel = new OpenLayers.Control.Panel({div : OpenLayers.Util.getElement('search_geometry_controls'), allowDepress: true});
-    this.mGeoControlPanel.allowDepress = true;
-    this.map_object.map.addControl(this.mGeoControlPanel);
+
     // add controls to the panel
-    this.mGeoControlPanel.addControls(
+    this.map_object.mNavigationPanel.addControls(
       [this.myDrawingControl]);
   },
   /* ------------------------------------------------------
@@ -107,7 +105,7 @@
       this.geoSearch.destroyFeatures(myOldFeatures);
     }
     this.writeWKT(event.feature.geometry);
-    this.mGeoControlPanel.addControls(
+    this.map_object.mNavigationPanel.addControls(
       [this.myModifyFeatureControl, this.myDestroyFeaturesControl]);
   },
 
@@ -117,9 +115,9 @@
 
   removeWKT: function(event) {
     document.getElementById('id_geometry').value = '';
-    OpenLayers.Util.removeItem(this.mGeoControlPanel.controls, this.myModifyFeatureControl);
-    OpenLayers.Util.removeItem(this.mGeoControlPanel.controls, this.myDestroyFeaturesControl);
-    this.mGeoControlPanel.redraw();
+    OpenLayers.Util.removeItem(this.map_object.mNavigationPanel.controls, this.myModifyFeatureControl);
+    OpenLayers.Util.removeItem(this.map_object.mNavigationPanel.controls, this.myDestroyFeaturesControl);
+    this.map_object.mNavigationPanel.redraw();
   }
 
 }; // prototype
