296713dce0c23ef36f05688d6f5175a389c39874
Dražen Odobašić <dodobas@candela-it.com>
2014-06-03 18:47:17 +0200

    Update csw mapping and view

--- resources/pycsw_catalogue_view.sql
+++ resources/pycsw_catalogue_view.sql
@@ -30,26 +30,58 @@ END;
 $$
 LANGUAGE 'plpgsql' VOLATILE STRICT;
 
-
-create or replace view pycsw_catalogue as SELECT catalogue_genericproduct.original_product_id AS product_id,
-    'csw:Record'::text AS typename,
+-- DROP view pycsw_catalogue;
+create or replace view pycsw_catalogue as SELECT
+    'csw:Record'::text AS csw_typename,
     'http://www.opengis.net/cat/csw/2.0.2'::text AS csw_schema,
-    catalogue_genericproduct.product_date AS insertdate,
+    catalogue_genericproduct.original_product_id AS csw_identifier,
+    dictionaries_satellite.name||' '||dictionaries_instrumenttype.name as csw_title,
+    dictionaries_instrumenttype.band_type as csw_alternatetitle,
+    dictionaries_institution.name as csw_organization_name,
+    'Earth Observation and Remote Sensing Data'::text as csw_topiccategory,
+    dictionaries_satellite.description||E'\n'||dictionaries_instrumenttype.description AS csw_abstract,
+    dictionaries_instrumenttype.keywords AS csw_keywords,
+    dictionaries_processinglevel.description as csw_lineage,
+    dictionaries_license.name as csw_license,
+    'EN'::text AS csw_language,
+    public.catalogue_genericsensorproduct.product_acquisition_start as csw_temp_begin,
+    public.catalogue_genericsensorproduct.product_acquisition_end as csw_temp_end,
+    'link,Show Product,http,http://41.74.158.4/showProduct/'::text || catalogue_genericproduct.original_product_id::text AS csw_link,
+
+    catalogue_genericproduct.product_date AS csw_insertdate,
     gen_xml_stub(catalogue_genericproduct.original_product_id) as csw_xml,
     ''::text AS csw_anytext,
-    ''::text AS csw_creator,
-    ''::text AS csw_identifier,
-    ''::text AS csw_language,
     'dataset'::text AS csw_type,
     st_astext(catalogue_genericproduct.spatial_coverage) AS wkt_geometry,
-    ''::text AS accessconstraints,
-    catalogue_genericproduct.metadata AS csw_metadata,
-    ''::text AS relation,
     catalogue_genericproduct.id,
-    catalogue_genericproduct.spatial_coverage,
-    'image'::text AS csw_keywords,
-    'test,test_desc,http,http://catalogue.sansa.org.za/showProduct/'::text || catalogue_genericproduct.original_product_id::text AS csw_links
-FROM catalogue_genericproduct
+    catalogue_genericproduct.spatial_coverage
+FROM
+  public.catalogue_genericproduct,
+  public.catalogue_genericimageryproduct,
+  public.catalogue_opticalproduct,
+  public.catalogue_genericsensorproduct,
+  public.dictionaries_opticalproductprofile,
+  public.dictionaries_satelliteinstrument,
+  public.dictionaries_institution,
+  public.dictionaries_satelliteinstrumentgroup,
+  public.dictionaries_satellite,
+  public.dictionaries_collection,
+  public.dictionaries_instrumenttype,
+  public.dictionaries_processinglevel,
+  public.dictionaries_license
+WHERE
+  catalogue_genericimageryproduct.genericproduct_ptr_id = catalogue_genericproduct.id AND
+  catalogue_opticalproduct.product_profile_id = dictionaries_opticalproductprofile.id AND
+  catalogue_genericsensorproduct.genericimageryproduct_ptr_id = catalogue_genericimageryproduct.genericproduct_ptr_id AND
+  catalogue_genericsensorproduct.genericimageryproduct_ptr_id = catalogue_opticalproduct.genericsensorproduct_ptr_id AND
+  dictionaries_satelliteinstrument.id = dictionaries_opticalproductprofile.satellite_instrument_id AND
+  dictionaries_satelliteinstrumentgroup.id = dictionaries_satelliteinstrument.satellite_instrument_group_id AND
+  dictionaries_satellite.id = dictionaries_satelliteinstrumentgroup.satellite_id AND
+  dictionaries_collection.institution_id = dictionaries_institution.id AND
+  dictionaries_collection.id = dictionaries_satellite.collection_id AND
+  dictionaries_instrumenttype.id = dictionaries_satelliteinstrumentgroup.instrument_type_id AND
+  dictionaries_processinglevel.id = dictionaries_instrumenttype.default_processing_level_id AND
+  dictionaries_license.id = dictionaries_satellite.license_type_id
 ORDER BY catalogue_genericproduct.id ASC
 LIMIT 100;
 
