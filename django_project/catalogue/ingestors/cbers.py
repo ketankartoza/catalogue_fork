70a8cd7a3224e7bf02d69d145d98b58e3f662cbd
Rischan Mafrur <rischanlab@gmail.com>
2016-04-06 11:07:37 +0700

    change theDate to date_stamp

--- django_project/catalogue/ingestors/cbers.py
+++ django_project/catalogue/ingestors/cbers.py
@@ -29,22 +29,22 @@ from dictionaries.models import (
 from catalogue.models import OpticalProduct
 
 
-def parse_date_time(theDate):
+def parse_date_time(date_stamp):
     """A helper method to create a date object from a CBERS time stamp.
 
-    :param theDate: Date in this format:
-    :type theDate: str
+    :param date_stamp: Date in this format:
+    :type date_stamp: str
 
     Example format from CBERS:`2015-12-03 10:40:23`
 
     :returns: A python datetime object.
     :rtype: datetime
     """
-    #print 'Parsing Date: %s\n' % theDate
-    start_year = theDate[0:4]
-    start_month = theDate[5:7]
-    start_day = theDate[8:10]
-    start_time = theDate[11:19]
+    #print 'Parsing Date: %s\n' % date_stamp
+    start_year = date_stamp[0:4]
+    start_month = date_stamp[5:7]
+    start_day = date_stamp[8:10]
+    start_time = date_stamp[11:19]
     tokens = start_time.split(':')
     start_hour = tokens[0]
     start_minute = tokens[1]
