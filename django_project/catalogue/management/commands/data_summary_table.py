71f769db8b3d5716f795a91b7bcaf1c83d870512
dimasciput <dimas.ciputra@gmail.com>
2016-04-28 16:51:59 +0700

    Dimas | Load data summary table from json
    - modify management command to generate json data

--- django_project/catalogue/management/commands/data_summary_table.py
+++ django_project/catalogue/management/commands/data_summary_table.py
@@ -1,61 +1,18 @@
-__author__ = 'rischan - <--rischan@kartoza.com-->'
-__date__ = '4/27/16'
-
-from django.db.models.query import QuerySet
-from django.contrib.gis.db import models
-from catalogue.dbhelpers import executeRAWSQL
+import json
 from django.db.models import Count
 from dictionaries.models import SatelliteInstrumentGroup
 from django.core.management.base import BaseCommand
 
+__author__ = 'rischan - <--rischan@kartoza.com-->, dimas - <--dimas@kartoza.com-->'
+__date__ = '4/27/16'
 
 halt_on_error = True
 # dir_path=('/home/web/django_project/media/')
 
+
 class Command(BaseCommand):
     args = 'path'
     help = 'Store data range data summary table from raw query'
-
-    def get_instrument_group_id(self):
-        instrument_group_id = executeRAWSQL("""
-            SELECT satellite_instrument_group_id
-            from dictionaries_satelliteinstrument;""")
-
-        inst_list = []
-        for i in range(len(instrument_group_id)):
-            inst_list.append(instrument_group_id[i]['satellite_instrument_group_id'])
-
-        return inst_list
-
-    def get_satellite_instrument_type(self, instrument_id=1):
-        data = executeRAWSQL("""
-            select dictionaries_satellite.operator_abbreviation as satellite,
-            dictionaries_instrumenttype.operator_abbreviation as instrument_type
-            from dictionaries_satellite, dictionaries_instrumenttype,
-            dictionaries_satelliteinstrumentgroup
-            where dictionaries_satellite.id=dictionaries_satelliteinstrumentgroup.satellite_id
-            AND dictionaries_instrumenttype.id=dictionaries_satelliteinstrumentgroup.instrument_type_id
-            and dictionaries_satelliteinstrumentgroup.id=
-            %(instrument_id)s;""", {'instrument_id': instrument_id})
-
-        return data
-
-    def get_product_per_year(self, inst_id):
-        myStats = executeRAWSQL("""
-            SELECT count(*) as count, extract(YEAR from gp.product_date)::int as year
-            FROM
-              catalogue_genericproduct gp, catalogue_genericimageryproduct gip,
-              catalogue_genericsensorproduct gsp, catalogue_opticalproduct op,
-              dictionaries_opticalproductprofile opp, dictionaries_satelliteinstrument si
-            WHERE
-              gip.genericproduct_ptr_id = gp.id AND
-              gsp.genericimageryproduct_ptr_id = gip.genericproduct_ptr_id AND
-              op.genericsensorproduct_ptr_id = gsp.genericimageryproduct_ptr_id AND
-              opp.id = op.product_profile_id AND opp.satellite_instrument_id = si.id
-            AND si.satellite_instrument_group_id=%(sensor_pk)s
-            GROUP BY extract(YEAR from gp.product_date)
-            ORDER BY year ASC;""", {'sensor_pk': inst_id})
-        return myStats
 
     def handle(self, *args, **options):
         """Implementation for command
@@ -73,26 +30,43 @@ class Command(BaseCommand):
             return
 
         path = args[0]
-        inst_list = list(set(self.get_instrument_group_id()))
 
-        for inst_id in inst_list:
-            file_name = "output.txt"
-            text_file = open(path + file_name, "a")
-            text_file.write("satellite: {}, instrument_type: {}\n".format(
-                self.get_satellite_instrument_type(inst_id)[0]['satellite'],
-                self.get_satellite_instrument_type(inst_id)[0]['instrument_type']))
-            print "write satellite and instrument type.."
-            try:
-                print "write min year..."
-                min_year = self.get_product_per_year(inst_id)[1]['year']
-                text_file.write("start_year : {}\n".format(min_year))
-            except:
-                text_file.write("start_year : - \n")
-            try:
-                print "write max year"
-                max_year = self.get_product_per_year(inst_id)[-1]['year']
-                text_file.write("end_year : {}\n".format(max_year))
-            except:
-                text_file.write("end_year : - \n")
-            continue
-            text_file.close()
+        file_name = 'output.json'
+
+        try:
+            json_file = open(path + file_name, 'w')
+            json_file.seek(0)
+
+            queryset = SatelliteInstrumentGroup.objects.annotate(
+                id__count=Count(
+                    'satelliteinstrument__opticalproductprofile__opticalproduct'))\
+                .order_by('satellite__name').filter(id__count__gt=0)
+
+            all_json_data = []
+
+            for result in queryset:
+                json_data = dict()
+                json_data['satellite_name'] = result.satellite.name
+                json_data['instrument_type'] = result.instrument_type.name
+                json_data['satellite_abbr'] = str(result.satellite.abbreviation)
+                json_data['instrument_abbr'] = str(result.instrument_type.abbreviation)
+                json_data['satellite_operator_abbr'] = str(result.satellite.operator_abbreviation)
+                json_data['instrument_operator_abbr'] = str(result.instrument_type.operator_abbreviation)
+                json_data['id__count'] = result.id__count
+                try:
+                    json_data['min_year'] = result.min_year()
+                except IndexError:
+                    json_data['min_year'] = '-'
+                try:
+                    json_data['max_year'] = result.max_year()
+                except IndexError:
+                    json_data['max_year'] = '-'
+                all_json_data.append(json_data)
+
+            json_file.write(json.dumps(all_json_data, indent=4))
+            json_file.truncate()
+            json_file.close()
+
+        except IOError:
+                print 'Error: can\'t find or read file'
+
